816197b3f5c447f3c34f0f6c565a34d364e07cd5
刘勇 <yong.liu7@transsion.com>
2022-06-12 21:26:20 +0800

    Revert "comment：优化脚手架登录和打印"
    This reverts commit cc6c19fefea27c7b10c4e33645bf1949e3ac50c8

--- project/DRP/test_case/run_code.py
+++ project/DRP/test_case/run_code.py
@@ -1,40 +1,44 @@
-import pytest, sys
+import pytest
 from public.data.unified_login.unified import *
 from libs.common.assert_ui import DomAssert, SQLAssert
 from public.libs.unified_login.login import Login
 from project.DRP.page_object.nav import NavPage
 from project.DRP.page_object.user import UserPage
-from libs.common.logger_ui import log
+from public.data.unified_login.unified import *
+
 
 class TestLogin:
-
-    @pytest.mark.smoke
     def test_001(self, drivers):
+        """用户管理-登录用户"""
+        user = Login(drivers)
+        user.login(drivers,account[0]['usernum'],account[0]['passwd'])
+        user = DomAssert(drivers)
+        user.assert_att("系统管理")
+        user.assert_url("http://10.250.112.166:9000/#/dashboard")
+        user = SQLAssert(drivers)
+        user.assert_sql(word='刘勇', sql='select name_zh from uc_user where enable_flag=1')
+
+    def test_002(self, drivers):
         """用户管理-查询用户"""
-        log.info("{} is start".format(sys._getframe().f_code.co_name))
         user = NavPage(drivers)
         user.click_gotonav("系统管理", "用户管理")
         user = UserPage(drivers)
-        user.search_user(jobnum=account[0]['username'])
+        user.search_user(jobnum='18650617')
         user.reset_account()
 
-    @pytest.mark.RT
-    def test_002(self, drivers):
+    def test_003(self, drivers):
         """用户管理-新建用户"""
-        log.info("{} is start".format(sys._getframe().f_code.co_name))
         user = UserPage(drivers)
         user.append_account("18650893")
 
-    @pytest.mark.smoke
-    def test_003(self, drivers):
+    def test_004(self, drivers):
         """用户管理-给新用户配置权限"""
-        log.info("{} is start".format(sys._getframe().f_code.co_name))
         user = UserPage(drivers)
         user.edit_Permission(
             jobnum="18650893",
             dimension={
                 '组织': ['itel事业部', '东非地区部'],
-                # '品牌': ['Infinix', 'itel', 'TECNO'],
+                '品牌': ['Infinix', 'itel', 'TECNO'],
                 # '区域': {'Infinix': ['利比亚', '土耳其']}
             }
         )
