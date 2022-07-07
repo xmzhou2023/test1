4fffdab84bc892b5acdafd5d8d3ccda09394a34b
18650493 <jiake.weng@transsion.com>
2022-07-07 16:19:27 +0800

    修改assert_ui.py断言文本存在的方法，增加文本精确匹配断言方法，增加页面控件是否存在断言方法

--- public/base/assert_ui.py
+++ public/base/assert_ui.py
@@ -1,4 +1,4 @@
-# from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.common.exceptions import TimeoutException
 import sys
@@ -10,25 +10,30 @@ from libs.common.connect_sql import *
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
+from public.base.basics import Base
+
 """     值校验的各种方法     """
+
 
 class ValueAssert(object):
 
     @allure.step("两值相等断言")
-    def value_assert_equal(a,b):
+    def value_assert_equal(a, b):
         try:
             assert a == b, logging.warning("断言失败: 两值不等 | a:{} b:{}".format(a, b))
             logging.info("断言成功: 两值相等 | a:{} b:{}".format(a, b))
         except Exception as e:
-            pass
+            logging.error(e)
+            raise
 
     @allure.step("两值不等断言")
-    def value_assert_Notequal(a,b):
+    def value_assert_Notequal(a, b):
         try:
             assert a != b, logging.warning("断言失败: 两值相等 | a:{} b:{}".format(a, b))
             logging.info("断言成功: 两值不等 | a:{} b:{}".format(a, b))
         except Exception as e:
-            pass
+            logging.error(e)
+            raise
 
     @allure.step("值为True值断言")
     def value_assert_True(x):
@@ -36,7 +41,8 @@ class ValueAssert(object):
             assert bool(x) is True, logging.warning("断言失败: 该值不为True值 | x:{}".format(x))
             logging.info("断言成功: 该值为True值 | x:{}".format(x))
         except Exception as e:
-            pass
+            logging.error(e)
+            raise
 
     @allure.step("值为False值断言")
     def value_assert_False(x):
@@ -44,23 +50,26 @@ class ValueAssert(object):
             assert bool(x) is False, logging.warning("断言失败: 该值不为False值 | x:{}".format(x))
             logging.info("断言成功: 该值为False值 | x:{}".format(x))
         except Exception as e:
-            pass
+            logging.error(e)
+            raise
 
     @allure.step("是本身断言")
-    def value_assert_Is(a,b):
+    def value_assert_Is(a, b):
         try:
             assert a is b, logging.warning("断言失败:  a 不是 b | a:{} b:{}".format(a, b))
             logging.info("断言成功: a 是 b | a:{} b:{}".format(a, b))
         except Exception as e:
-            pass
+            logging.error(e)
+            raise
 
     @allure.step("不是本身断言")
-    def value_assert_IsNot(a,b):
+    def value_assert_IsNot(a, b):
         try:
             assert a is not b, logging.warning("断言失败: a 是 b | a:{} b:{}".format(a, b))
             logging.info("断言成功: a 不是 b | a:{} b:{}".format(a, b))
         except Exception as e:
-            pass
+            logging.error(e)
+            raise
 
     @allure.step("值为None值断言")
     def value_assert_IsNone(x):
@@ -68,7 +77,8 @@ class ValueAssert(object):
             assert x is None, logging.warning("断言失败: 该值不为None | x:{}".format(x))
             logging.info("断言成功: 该值为None | x:{}".format(x))
         except Exception as e:
-            pass
+            logging.error(e)
+            raise
 
     @allure.step("值不为None值断言")
     def value_assert_IsNoneNot(x):
@@ -76,41 +86,48 @@ class ValueAssert(object):
             assert x is not None, logging.warning("断言失败: 该值为None | x:{}".format(x))
             logging.info("断言成功: 该值不为None | x:{}".format(x))
         except Exception as e:
-            pass
+            logging.error(e)
+            raise
 
     @allure.step("是包含断言")
-    def value_assert_In(a,b):
+    def value_assert_In(a, b):
         try:
             assert a in b, logging.warning("断言失败: a 不包含 b | a:{} b:{}".format(a, b))
             logging.info("断言成功: a 包含 b | a:{} b:{}".format(a, b))
         except Exception as e:
-            pass
+            logging.error(e)
+            raise
 
     @allure.step("不是包含断言")
-    def value_assert_InNot(a,b):
+    def value_assert_InNot(a, b):
         try:
             assert a not in b, logging.warning("断言失败: a 包含 b | a: {} b:{}".format(a, b))
             logging.info("断言成功: a 不包含 b | a: {} b: {}".format(a, b))
         except Exception as e:
-            pass
+            logging.error(e)
+            raise
 
     @allure.step("判断值为指定类型断言")
-    def value_assert_Instance(a,b):
+    def value_assert_Instance(a, b):
         try:
-            assert isinstance(a,b), logging.warning("断言失败: a: {} 类型：{}".format(a, b))
+            assert isinstance(a, b), logging.warning("断言失败: a: {} 类型：{}".format(a, b))
             logging.info("断言成功: a: {} 类型： {}".format(a, b))
         except Exception as e:
-            pass
+            logging.error(e)
+            raise
 
     @allure.step("判断值不是指定类型断言")
-    def value_assert_IsInstanceNot(a,b):
+    def value_assert_IsInstanceNot(a, b):
         try:
-            assert not isinstance(a,b), logging.warning("断言失败: a: {} 类型：{}".format(a, b))
+            assert not isinstance(a, b), logging.warning("断言失败: a: {} 类型：{}".format(a, b))
             logging.info("断言成功: a: {} 类型：{}".format(a, b))
         except Exception as e:
-            pass
+            logging.error(e)
+            raise
+
 
 """     页面元素校验的方法     """
+
 
 class DomAssert(object):
 
@@ -126,17 +143,32 @@ class DomAssert(object):
             assert (word in value), logging.warning("断言失败：运行系统与预期不一致 |  当前系统: {}".format(value))
             logging.info("断言成功：运行系统与预期一致 | 当前系统: {}".format(value))
         except Exception as e:
-            return e
+            logging.error(e)
+            raise
 
-    @allure.step("值为True值断言")
+    @allure.step("存在某文字断言")
     def assert_att(self, word):
         """页面是否存在某文字"""
         try:
-            att = self.driver.find_element(By.XPATH,'//*[contains(text(),{})]'.format(word)).text
-            assert word in att, logging.warning("断言失败：页面不存在该标识 | 当前页面关键字: {}".format(att.replace("\n","|")))
-            logging.info("断言成功：页面存在该标识 | 当前页面关键字: {}".format(att.replace("\n","|")))
+            Base(self.driver).base_get_img('result')
+            att = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH, "//*[normalize-space(text())='{}']".format(word)))).text
+            assert word in att, logging.warning("断言失败：页面不存在该标识 | 当前页面关键字: {}".format(att.replace("\n", "|")))
+            logging.info("断言成功：页面存在该标识 | 当前页面关键字: {}".format(att.replace("\n", "|")))
         except Exception as e:
-            return e
+            logging.error(e)
+            raise
+
+    def assert_exact_att(self, word):
+        """精确匹配：页面是否存在某文字"""
+        try:
+            Base(self.driver).base_get_img('result')
+            att = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH, "//*[normalize-space(text())='{}']".format(word)))).text
+            assert word in att, logging.warning("断言失败：页面不存在该标识{} | 关键字:{}".format(att, word))
+            logging.info("断言成功：页面存在该标识{} | 关键字:{}".format(att, word))
+        except:
+            raise
 
     @allure.step("值为True值断言")
     def assert_title(self, word):
@@ -146,7 +178,8 @@ class DomAssert(object):
             assert word in att.title, logging.warning("断言失败：标题为预期不符 | 标题: {}".format(att.title))
             logging.info("断言成功：标题为预期一致 | 标题: {}".format(att.title))
         except Exception as e:
-            return e
+            logging.error(e)
+            raise
 
     @allure.step("值为True值断言")
     def assert_url(self, word):
@@ -156,7 +189,8 @@ class DomAssert(object):
             assert word in att, logging.warning("断言失败：URL为预期不一致 | URL: {}".format(att))
             logging.info("断言成功：URL为预期一致 | URL: {}".format(att))
         except Exception as e:
-            return e
+            logging.error(e)
+            raise
 
     @allure.step("值为True值断言")
     def assert_page_source(self, word):
@@ -166,7 +200,8 @@ class DomAssert(object):
             assert word not in value, logging.warning("断言失败：页面包含此标识 | 标识: {}".format(value))
             logging.info("断言成功：页面不包含此标识| 标识: {}".format(value))
         except Exception as e:
-            return e
+            logging.error(e)
+            raise
 
     @allure.step("值为True值断言")
     def assert_select(self, element, word):
@@ -178,7 +213,8 @@ class DomAssert(object):
             assert word == value, logging.warning("断言失败：下拉选择框不是该值 | 当前值: {}".format(value))
             logging.info("断言成功：下拉选择框是该值| 当前值: {}".format(value))
         except Exception as e:
-            return e
+            logging.error(e)
+            raise
 
     @allure.step("值为True值断言")
     def assert_filename(self, element, word):
@@ -190,7 +226,8 @@ class DomAssert(object):
             assert word in value, logging.warning("断言失败：选择上传不是该文件 | 当前文件: {}".format(value))
             logging.info("断言成功：选择上传是该文件| 当前文件: {}".format(value))
         except Exception as e:
-            return e
+            logging.error(e)
+            raise
 
     @allure.step("值为True值断言")
     def assert_domcolor(self, element, color):
@@ -203,7 +240,8 @@ class DomAssert(object):
             assert color == value, logging.warning("断言失败：颜色不符合预期 | 当前元素颜色: {}".format(value))
             logging.info("断言成功：颜色符合预期| 当前元素颜色: {}".format(value))
         except Exception as e:
-            return e
+            logging.error(e)
+            raise
 
     @allure.step("值为True值断言")
     def assert_alerttext(self, element, content):
@@ -216,9 +254,27 @@ class DomAssert(object):
             assert content == value, logging.warning("断言失败：弹窗内容不符合预期 | 当前弹窗内容: {}".format(value))
             logging.info("断言成功：弹窗内容符合预期 | 当前弹窗内容: {}".format(value))
         except Exception as e:
-            return e
+            logging.error(e)
+            raise
+
+    @allure.step("值为True值断言")
+    def assert_control(self, element, result=True):
+        """
+        断言：页面是否存在控件
+        @element：元素定位
+        @result：断言结果，True表示断言存在； False表示断言不存在
+        @choice：元素定位
+        """
+        control = Base(self.driver).element_exist(element)
+        try:
+            assert control is result, logging.warning('断言失败，元素：{}存在结果与期望结果：{}不符'.format(element, result))
+            logging.info('断言成功，元素：{}存在结果为：{}'.format(element, result))
+        except Exception as e:
+            logging.error(e)
+            raise
 
 """     数据库断言     """
+
 
 class SQLAssert(object):
     def __init__(self, name, env):
@@ -228,7 +284,7 @@ class SQLAssert(object):
     @allure.step("值为True值断言")
     def assert_sql(self, word, sql):
         """页面是否存在某文字"""
-        database = SQL(self.name,self.env)
+        database = SQL(self.name, self.env)
         sql_list = database.query_db(sql)
         sql_colum = []
         for i in sql_list:
@@ -237,23 +293,22 @@ class SQLAssert(object):
             assert word in sql_colum, logging.warning("断言失败: 该内容不存在数据列表中 | word:{}".format(word))
             logging.info("断言成功: 该内容存在数据列表中 | word:{}".format(word))
         except Exception as e:
-            return e
+            logging.error(e)
+            raise
+
 
 if __name__ == "__main__":
-   # print(value_assert_equal(1,2))
-   # print(value_assert_Notequal(1,2))
-   # print(value_assert_True(0))
-   # print(value_assert_False(1))
-   # print(value_assert_Is(2, 2))
-   # print(value_assert_IsNot(2,2))
-   # print(value_assert_IsNone(None))
-   # print(value_assert_IsNoneNot(1))
-   # print(value_assert_In(3, {3,2}))
-   # print(value_assert_InNot(3, {3,2}))
-   # print(value_assert_Instance("abc",str))
-   # print(value_assert_IsInstanceNot(2,int))
-   a = SQLAssert('DRP','test')
-   print(a.assert_sql(word='刘勇', sql='select name_zh from uc_user where enable_flag=1'))
-
-
-
+    # print(value_assert_equal(1,2))
+    # print(value_assert_Notequal(1,2))
+    # print(value_assert_True(0))
+    # print(value_assert_False(1))
+    # print(value_assert_Is(2, 2))
+    # print(value_assert_IsNot(2,2))
+    # print(value_assert_IsNone(None))
+    # print(value_assert_IsNoneNot(1))
+    # print(value_assert_In(3, {3,2}))
+    # print(value_assert_InNot(3, {3,2}))
+    # print(value_assert_Instance("abc",str))
+    # print(value_assert_IsInstanceNot(2,int))
+    a = SQLAssert('DRP', 'test')
+    print(a.assert_sql(word='刘勇', sql='select name_zh from uc_user where enable_flag=1'))
