1bc44d4f12f905d5d9402ebfdac87d387697e903
Evan <yong.liu7@transsion.com>
2022-07-11 16:49:52 +0800

    添加指定环境项目环境运行

--- project/TBM/test_case/conftest.py
+++ project/TBM/test_case/conftest.py
@@ -1,16 +1,20 @@
 import pytest, logging
 from public.data.unified_login.unified import *
-from public.base.assert_ui import DomAssert, SQLAssert
+from public.base.assert_ui import *
 from public.libs.unified_login.login import Login
 from libs.common.read_config import *
 
 pro_name = os.path.dirname(os.path.dirname(os.path.abspath(__file__))).split('\\')[-1]
-pro_env = 'test' # 需要手动配置测试环境
-ini = ReadConfig(pro_name, pro_env)
 
 @pytest.fixture(scope='session',autouse=True)
-def test_login(drivers):
-    """登录用户"""
+def __init__(drivers, env_name):
+    """初始化"""
+    global pro_env
+    pro_env = env_name
+    logging.info("【{}】项目【{}】环境- UI自动化开始执行".format(pro_name, pro_env))
+    ini = ReadConfig(pro_name, pro_env)
+
+    """使用统一登录"""
     logging.info("前置条件：传音统一登录开始")
     user = Login(drivers)
     user.login(drivers, ini.url, account[2]['usernum'], account[2]['passwd'])
