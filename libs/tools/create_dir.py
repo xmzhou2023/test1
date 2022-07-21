df01e7579099d8d0907d7fab0f486d47b77bb017
Evan <yong.liu7@transsion.com>
2022-07-21 16:47:45 +0800

    提交DRP模块说明

--- libs/tools/create_dir.py
+++ libs/tools/create_dir.py
@@ -4,7 +4,7 @@ import logging
 import os
 import shutil
 
-PERO_PATH = os.path.join(PEROJECT_PATH, 'TEST')
+PERO_PATH = os.path.join(PEROJECT_PATH, 'HAHA')
 # 创建文件依据
 path = os.path.join('template/modle_name.csv')
 
