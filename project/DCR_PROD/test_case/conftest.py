af888d39411ccee59ba141bc6aba789cd4205fc0
18648901 <hongmei.li@transsion.com>
2022-07-28 11:37:53 +0800

    修改GLOBAL环境的InboundReceipt文件的断言问题

---
+++ project/DCR_PROD/test_case/conftest.py
@@ -0,0 +1,30 @@
+import pytest, logging
+from public.data.unified_login.unified import *
+from public.base.assert_ui import *
+from public.libs.unified_login.login import Login
+from libs.common.read_config import *
+
+pro_name = os.path.dirname(os.path.dirname(os.path.abspath(__file__))).split('\\')[-1]
+pro_env = 'prod_globel' # 需要手动配置测试环境
+ini = ReadConfig(pro_name, pro_env)
+
+@pytest.fixture(scope='session',autouse=True)
+def test_login(drivers):
+    """登录用户"""
+    logging.info("前置条件：传音统一登录开始")
+    user = Login(drivers)
+    user.dcr_login(drivers,ini.url, account[3]['usernum'], account[3]['passwd'])
+    logging.info("前置条件：传音统一登录成功")
+
+
+
+pro_name = os.path.basename(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+@pytest.fixture(scope='session',autouse=True)
+def __init__(drivers, env_name):
+    """初始化"""
+    global pro_env
+    #pro_env = env_name
+    pro_env = "prod_globel"
+    logging.info("【{}】项目【{}】环境- UI自动化开始执行".format(pro_name, pro_env))
+
