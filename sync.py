d4c08845886b63f2aa0d1bb0f5d99bd8c4a629f1
Evan <yong.liu7@transsion.com>
2022-08-09 15:49:37 +0800

    测试脚本

--- sync.py
+++ sync.py
@@ -632,6 +632,7 @@ def update_data(type, sql_data, data_list, parm=None):
             python_list.append(data_list[case_code]['title'].replace('\"','').replace('\'',''))
             python_list.append(data_list[case_code]['description'].replace('\"','').replace('\'',''))
             python_list.append(str(case_level[data_list[case_code]['severity']]))
+        print(python_list)
 
         case_list_py = sorted(case_list_py.items())
         print(case_list_py)
