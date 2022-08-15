d350b29c2f670ad0d3b451d4b196c4695e599409
Evan <yong.liu7@transsion.com>
2022-08-15 16:42:23 +0800

    清除master

--- public/data/unified_login/unified.py
+++
@@ -1,4 +0,0 @@
-from libs.common.read_csv import *
-
-account = readCsvDict(os.path.join(PUBLIC_DATA_PATH, 'account.csv')) # 统一认证登录
-account_srm= readCsvDict(os.path.join(PUBLIC_DATA_PATH, 'account_srm.csv')) # srm统一认证登录
