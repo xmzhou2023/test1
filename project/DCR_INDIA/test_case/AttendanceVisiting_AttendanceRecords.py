d9717ecf7e4b97bd7cd4a7c8611dd2f3f660d1e5
18648901 <hongmei.li@transsion.com>
2022-08-15 14:20:06 +0800

    修改DCR_INDIA生产环境用例规范又复原的问题

--- project/DCR_INDIA/test_case/AttendanceVisiting_AttendanceRecords.py
+++ project/DCR_INDIA/test_case/AttendanceVisiting_AttendanceRecords.py
@@ -18,8 +18,6 @@ class TestQueryAttendanceRecord:
     def test_001_001(self, drivers):
         user = LoginPage(drivers)
         #user.dcr_login(drivers, "testsupervisor", "dcr123456")
-        #get_home_page = user.get_home_page_text()
-        #ValueAssert.value_assert_equal("Home Page-Customer", get_home_page)
 
         """考勤管理-打开考勤记录页面"""
         user.click_gotomenu("Attendance & Visiting", "Attendance Records")
