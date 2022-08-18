6c6fca8c646fc53a535b584fa68b7aca74fe39a4
Evan <yong.liu7@transsion.com>
2022-08-18 18:15:50 +0800

    提交dcr

--- project/DCR/page_object/ShopManagement_ShopManagement.py
+++ project/DCR/page_object/ShopManagement_ShopManagement.py
@@ -18,6 +18,7 @@ class ShopManagementPage(Base):
 
     @allure.step("新建门店，输入门店名称")
     def input_shop_name(self, content):
+        Base.presence_sleep_dcr(self, user['Shop Name'])
         self.input_text(user['Shop Name'], txt=content)
 
     @allure.step("新建门店，输入联系人")
@@ -97,7 +98,7 @@ class ShopManagementPage(Base):
     @allure.step("新建或者编辑门店时，点击提交")
     def click_submit(self):
         self.is_click_dcr(user['Submit'], "Submit")
-        sleep(5)
+        sleep(1)
 
     @allure.step("新建门店时，门店名称后缀随机生成")
     def shop_random(self):
@@ -110,6 +111,7 @@ class ShopManagementPage(Base):
         self.is_click_dcr(user['点击门店输入框'])
         self.input_text(user['门店输入框输入'], txt=content)
         sleep(3)
+        Base.presence_sleep_dcr(self, user['选中门店值1'], content1)
         self.is_click(user['选中门店值1'], content1)
 
     @allure.step("根据门店名称查询，最近扩展门店品牌的的门店ID")
@@ -190,6 +192,7 @@ class ShopManagementPage(Base):
 
     @allure.step("获取列表Status文本")
     def get_shop_status_text(self):
+        Base.presence_sleep_dcr(self, user['获取Status文本'])
         get_status = self.element_text(user['获取Status文本'])
         return get_status
 
@@ -265,20 +268,153 @@ class ShopManagementPage(Base):
         sleep(1.5)
         self.is_click_dcr(user['Extend Commercial Value'], "KA")
 
-    #禁用门店
-    @allure.step("点击禁用门店操作")
-    def click_disable_confirm(self):
-        Base.presence_sleep_dcr(self, user['Disable'])
-        self.is_click(user['Disable'])
-        sleep(2)
-        Base.presence_sleep_dcr(self, user['Disable Confirm'])
-        self.is_click(user['Disable Confirm'])
-
     @allure.step("关闭门店管理菜单")
     def click_close_shop_management(self):
         self.is_click(user['关闭门店管理菜单'])
         sleep(2)
 
+
+    #禁用门店
+    @allure.step("点击禁用门店按钮")
+    def click_disable_confirm(self):
+        Base.presence_sleep_dcr(self, user['Disable'])
+        self.is_click(user['Disable'])
+        sleep(1.5)
+        Base.presence_sleep_dcr(self, user['Disable Confirm'])
+        self.is_click(user['Disable Confirm'])
+        sleep(1)
+
+    #启用门店Disabled
+    @allure.step("点击启用门店按钮")
+    def click_enable_confirm(self):
+        Base.presence_sleep_dcr(self, user['Enable'])
+        self.is_click(user['Enable'])
+        sleep(1.5)
+        Base.presence_sleep_dcr(self, user['Enable Confirm'])
+        self.is_click(user['Enable Confirm'])
+        sleep(1)
+
+    @allure.step("Status筛选项，筛选禁用或启用状态")
+    def click_status_filter(self, status):
+        self.is_click(user['门店列表筛选状态'])
+        sleep(1)
+        Base.presence_sleep_dcr(self, user['筛选后选中禁用值'], status)
+        self.is_click(user['筛选后选中禁用值'], status)
+
+    @allure.step("点击Status筛选项后，点击Status属性名称，释放光标位置")
+    def click_status_attribute(self):
+        self.is_click(user['Click Status Attribute'])
+        sleep(1)
+
+    #View查看门店
+    @allure.step("门店管理页面，点击View查看门店详情")
+    def click_view_shop(self):
+        self.is_click(user['View Shop'])
+        sleep(2)
+
+    @allure.step("门店管理页面，获取列表Shop ID段内容")
+    def get_list_shop_id_text(self):
+        Base.presence_sleep_dcr(self, user['Get list Shop ID Text'])
+        list_shop_id = self.element_text(user['Get list Shop ID Text'])
+        return list_shop_id
+
+    @allure.step("门店管理页面，获取列表Shop Name字段内容")
+    def get_list_shop_name_text(self):
+        Base.presence_sleep_dcr(self, user['Get list Shop Name Text'])
+        list_shop_name1 = self.element_text(user['Get list Shop Name Text'])
+        return list_shop_name1
+
+    @allure.step("门店管理页面，获取列表Contact Name字段内容")
+    def get_list_public_id_text(self):
+        self.scroll_into_view(user['Get list Public ID Text'])
+        contact_name = self.element_text(user['Get list Public ID Text'])
+        return contact_name
+
+    @allure.step("门店管理页面，获取列表Contact Name字段内容")
+    def get_list_contact_name_text(self):
+        self.scroll_into_view(user['Get list Public ID Text'])
+        contact_name = self.element_text(user['Get list Contact Name Text'])
+        return contact_name
+
+    @allure.step("门店管理页面，获取列表Contact no字段内容")
+    def get_list_contact_no_text(self):
+        contact_no = self.element_text(user['Get list Contact No Text'])
+        return contact_no
+
+    @allure.step("View详情页，获取Shop ID字段内容")
+    def get_view_shop_id_text(self):
+        self.scroll_into_view(user['Get view Shop ID Text'])
+        view_shop_id = self.element_input_text(user['Get view Shop ID Text'])
+        return view_shop_id
+
+    @allure.step("View详情页，获取Shop Name字段内容")
+    def get_view_shop_name_text(self):
+        view_shop_name = self.element_input_text(user['Get View Shop Name Text'])
+        return view_shop_name
+
+    @allure.step("View详情页，获取Contact Name字段内容")
+    def get_view_contact_name_text(self):
+        view_contact_name = self.element_input_text(user['Get View Contact Name Text'])
+        return view_contact_name
+
+    @allure.step("View详情页，获取Contact No字段内容")
+    def get_view_contact_no_text(self):
+        view_contact_no = self.element_input_text(user['Get View Contact No Text'])
+        return view_contact_no
+
+    @allure.step("View详情页，获取Public ID字段内容")
+    def get_view_public_id_text(self):
+        view_public_id = self.element_input_text(user['Get View Public ID Text'])
+        return view_public_id
+
+    @allure.step("View详情页面，关闭门店View页面")
+    def click_close_shop_view(self):
+        self.is_click(user['Close Shop View'])
+        sleep(3)
+
+    #查询门店信息
+    @allure.step("点击Unfold 展开筛选项")
+    def click_unfold(self):
+        self.is_click(user['Unfold'])
+        sleep(2)
+
+    @allure.step("点击Fold收起筛选项")
+    def click_fold(self):
+        self.is_click(user['Fold'])
+
+    @allure.step("输入Create Date开始日期与截止日期")
+    def input_create_date(self, context, context1):
+        self.is_click(user['Create Start Date'])
+        self.input_text(user['Create Start Date'], txt=context)
+        sleep(0.5)
+        self.is_click(user['Create End Date'])
+        self.input_text(user['Create End Date'], txt=context1)
+
+    @allure.step("获取Total分页总数")
+    def get_total_text(self):
+        total = self.element_text(user['Get Total'])
+        total1 = total[6:]
+        return total1
+
+    @allure.step("断言分页总数是否存在数据")
+    def assert_total(self, total):
+        if int(total) > 1:
+            logging.info("筛选考勤记录列表，分页总条数大于1，能查询到考勤记录数Total:{}".format(total))
+        else:
+            logging.info("筛选考勤记录列表，分页总条数为1，未查询到考勤记录数Total:{}:".format(total))
+
+    @allure.step("获取列表品牌字段内容")
+    def get_list_brand_text(self):
+        get_list_brand = self.element_text(user['Get list Brand Text'])
+        return get_list_brand
+
+
+    #编辑门店信息
+    @allure.step("门店列表页面，点击编辑门店功能")
+    def click_edit_shop(self):
+        Base.presence_sleep_dcr(self, user['Edit Shop'])
+        self.is_click(user['Edit Shop'])
+        sleep(2)
 
 
 if __name__ == '__main__':
--- project/DCR/test_case/ShopManagement_ShopManagement.py
+++ project/DCR/test_case/ShopManagement_ShopManagement.py
@@ -1,3 +1,4 @@
+import logging
 from project.DCR.page_object.ShopManagement_ShopManagement import ShopManagementPage
 from project.DCR.page_object.Center_Component import LoginPage
 from public.base.assert_ui import *
@@ -48,7 +49,7 @@ class TestAddShop:
             "select public_code,shop_name from  t_retail_shop_base where creator=99940 order by creation_time desc limit 1")
         shop_id = shop_data[0].get("public_code")
         shop_name = shop_data[0].get("shop_name")
-        """获取列表新建的门店ID与门店名称文本内容"""
+        """筛选新建的门店ID与门店名称文本内容"""
         add_shop.input_query_shop_name(shop_name, shop_name)
         add_shop.click_query_search()
 
@@ -65,7 +66,7 @@ class TestAddShop:
 
 
 @allure.feature("门店管理-门店管理(global)")
-class TestEditShop:
+class TestExpandBrandShop:
     @allure.story("扩展门店品牌")
     @allure.title("门店管理页面，对新增的门店进行扩展itel品牌操作")
     @allure.description("门店管理页面，对新增的门店进行扩展品牌操作，扩展itel品牌提交后，列表展示扩展的门店品牌")
@@ -151,6 +152,179 @@ class TestDisableShop:
         disable.click_close_shop_management()
 
 
+@allure.feature("门店管理-门店管理(global)")
+class TestQueryGlobalShop:
+    @allure.story("查询全球门店")
+    @allure.title("门店管理页面，按门店ID与状态条件筛选全球门店列表数据加载是否正常")
+    @allure.description("门店管理页面，按门店ID与状态条件筛选全球门店列表数据加载是否正常")
+    @allure.severity("normal")  # 分别为3种类型等级：critical\normal\minor
+    def test_004_001(self, drivers):
+        user = LoginPage(drivers)
+        user.initialize_login(drivers, "lhmadmin", "dcr123456")
+        """销售管理菜单-出库单-筛选出库单用例"""
+        user.click_gotomenu("Shop Management", "Shop Management(Global)")
+
+        """实例化ShopManagementPage类，调用页面元素方法"""
+        query = ShopManagementPage(drivers)
+        today = Base(drivers).get_datetime_today()
+        logging.info("打印当前日期：{}".format(today))
+
+        query.click_unfold()
+        """门店列表，按日期筛选门店记录"""
+        query.input_create_date("2022-08-01", today)
+        query.click_status_attribute()
+        """点击查询按钮"""
+        query.click_query_search()
+        query.click_fold()
+
+        """断言筛选后列表是否存在门店记录，分页总数是否有记录"""
+        get_list_shop_id = query.get_list_shop_id_text()
+        get_list_shop_name = query.get_list_shop_name_text()
+        get_list_brand = query.get_list_brand_text()
+        get_public_id = query.get_list_public_id_text()
+
+        ValueAssert.value_assert_IsNoneNot(get_list_shop_id)
+        ValueAssert.value_assert_IsNoneNot(get_list_shop_name)
+        ValueAssert.value_assert_IsNoneNot(get_list_brand)
+        ValueAssert.value_assert_IsNoneNot(get_public_id)
+        total = query.get_total_text()
+        query.assert_total(total)
+        query.click_close_shop_management()
+
+
+    @allure.story("查询全球门店")
+    @allure.title("门店管理页面，查看View门店详情信息是否正确")
+    @allure.description("门店管理页面，查看View门店详情信息是否正确")
+    @allure.severity("normal")  # 分别为3种类型等级：critical\normal\minor
+    def test_004_002(self, drivers):
+        user4 = LoginPage(drivers)
+        user4.initialize_login(drivers, "lhmadmin", "dcr123456")
+        """销售管理菜单-出库单-筛选出库单用例"""
+        user4.click_gotomenu("Shop Management", "Shop Management(Global)")
+        """实例化ShopManagementPage类，调用页面元素方法"""
+        view = ShopManagementPage(drivers)
+        # """从数据库查询最近新建的门店ID"""
+        # user = SQL('DCR', 'test')
+        # shop_data = user.query_db(
+        #     "select public_code, shop_name from t_retail_shop_base where creator=99940 order by creation_time desc limit 1")
+        # shop_id = shop_data[0].get("public_code")
+        # shop_name = shop_data[0].get("shop_name")
+        # logging.info("从数据库表查询最近新建的Shop ID{}".format(shop_id))
+        # logging.info("从数据库表查询最近新建的Shop ID{}".format(shop_name))
+
+        """筛选新建的门店ID记录"""
+        view.input_query_shop_name("BD026498", "BD026498")
+        view.click_status_filter("Disabled")
+        view.click_status_attribute()
+        view.click_query_search()
+
+        list_shop_name = view.get_list_shop_name_text()
+        list_public_id = view.get_list_public_id_text()
+        list_contact_name = view.get_list_contact_name_text()
+        list_contact_no = view.get_list_contact_no_text()
+
+        """点击view打开门店详情页"""
+        view.click_view_shop()
+        view_shop_name = view.get_view_shop_name_text()
+        view_contact_name = view.get_view_contact_name_text()
+        view_contact_no = view.get_view_contact_no_text()
+        view_public_id = view.get_view_public_id_text()
+
+        """断言获取列表的门店基本信息与 查看view详情页面的信息是否一致"""
+        ValueAssert.value_assert_equal(list_shop_name, view_shop_name)
+        ValueAssert.value_assert_equal(list_contact_name, view_contact_name)
+        ValueAssert.value_assert_equal(list_contact_no, view_contact_no)
+        ValueAssert.value_assert_equal(list_public_id, view_public_id)
+        """点击关闭view页面"""
+        view.click_close_shop_view()
+        view.click_close_shop_management()
+
+
+@allure.feature("门店管理-门店管理(global)")
+class TestEditShop:
+    @allure.story("编辑门店信息")
+    @allure.title("门店管理页面，Edit编辑门店信息")
+    @allure.description("门店管理页面，Edit编辑门店信息，提交后，返回列表显示编辑后的门店信息")
+    @allure.severity("normal")  # 分别为3种类型等级：critical\normal\minor
+    def test_005_001(self, drivers):
+        user5 = LoginPage(drivers)
+        user5.initialize_login(drivers, "lhmadmin", "dcr123456")
+
+        """销售管理菜单-出库单-筛选出库单用例"""
+        user5.click_gotomenu("Shop Management", "Shop Management(Global)")
+        """实例化ShopManagementPage类，调用页面元素方法"""
+        edit = ShopManagementPage(drivers)
+        # """从数据库查询最近新建的门店ID"""
+        # user = SQL('DCR', 'test')
+        # shop_data = user.query_db(
+        #     "select public_code, shop_name from t_retail_shop_base where creator=99940 order by creation_time desc limit 1")
+        # shop_id = shop_data[0].get("public_code")
+        # shop_name = shop_data[0].get("shop_name")
+        # logging.info("从数据库表查询最近新建的Shop ID{}".format(shop_id))
+        # logging.info("从数据库表查询最近新建的Shop ID{}".format(shop_name))
+        """筛选新建的门店ID记录"""
+        edit.input_query_shop_name("BD026498", "BD026498")
+        edit.click_status_filter("Disabled")
+        edit.click_status_attribute()
+        edit.click_query_search()
+
+        """点击Edit按钮"""
+        edit.click_edit_shop()
+        """门店名称后面增加随机数字"""
+        num = edit.shop_random()
+        edit.input_shop_name("test_shop"+num)
+        edit_shop_name = "test_shop"+num
+        logging.info("获取编辑后的shop name：{}".format(edit_shop_name))
+
+        """点击提交按钮"""
+        edit.click_submit()
+        DomAssert(drivers).assert_att("Edited Successfully")
+        sleep(2)
+        edit.click_close_shop_management()
+
+
+@allure.feature("门店管理-门店管理(global)")
+class TestEnableShop:
+    @allure.story("启用门店")
+    @allure.title("门店管理页面，对禁用的门店，进行启用操作")
+    @allure.description("门店管理页面，对禁用的门店，进行启用操作")
+    @allure.severity("minor")  # 分别为3种类型等级：critical\normal\minor
+    def test_006_001(self, drivers):
+        user6 = LoginPage(drivers)
+        user6.initialize_login(drivers, "lhmadmin", "dcr123456")
+        """销售管理菜单-出库单-筛选出库单用例  BD026498"""
+        user6.click_gotomenu("Shop Management", "Shop Management(Global)")
+        """实例化ShopManagementPage类，调用页面元素方法"""
+        enable = ShopManagementPage(drivers)
+
+        """筛选门店，然后启用门店操作"""
+        enable.input_query_shop_name("BD026498", "BD026498")
+        enable.click_status_filter("Disabled")
+        enable.click_status_attribute()
+        enable.click_query_search()
+        """获取列表最新的门店状态"""
+        get_status1 = enable.get_shop_status_text()
+        if "Enabled" == get_status1:
+            enable.click_first_checkbox()
+            enable.click_more_option()
+            enable.click_disable_confirm()
+            DomAssert(drivers).assert_att("Successfully")
+            sleep(5)
+            """获取列表最新的门店状态"""
+            get_status2 = enable.get_shop_status_text()
+            ValueAssert.value_assert_equal("Disabled", get_status2)
+        else:
+            enable.click_first_checkbox()
+            enable.click_more_option()
+            enable.click_enable_confirm()
+            DomAssert(drivers).assert_att("Successfully")
+            sleep(5)
+            get_status3 = enable.get_shop_status_text()
+            """断言启用操作后，门店列表是否更新为Enabled状态 """
+            ValueAssert.value_assert_equal("Enabled", get_status3)
+        enable.click_close_shop_management()
+
+
 #暂时无删除功能，用例留着
 # @allure.feature("门店管理-门店管理(global)")
 # class TestDeleteShop:
@@ -158,7 +332,7 @@ class TestDisableShop:
 #     @allure.title("门店管理页面，对新增的扩展品牌门店进行删除操作")
 #     @allure.description("门店管理页面，对新增的扩展品牌门店进行删除操作")
 #     @allure.severity("blocker")  # 分别为5种类型等级：blocker\critical\normal\minor\trivial
-#     def test_001_003(self, drivers):
+#     def test_008_001(self, drivers):
 #         """实例化ShopManagementPage类，调用页面元素方法"""
 #         del_shop = ShopManagementPage(drivers)
 #         """选中门店进行删除扩展门店"""
@@ -179,8 +353,7 @@ class TestDisableShop:
 #         shop_name2 = del_shop.get_shop_name_text()
 #         ValueAssert.value_assert_InNot(shop_id1, shop_id2)
 #         ValueAssert.value_assert_InNot(shop_name1, shop_name2)
-#         sleep(1)
+#         sleep(1)  #TestExportShop
 
-
-if __name__ == '__mainShopManagement_ShopManagement__':
-    pytest.main(['.py'])
+if __name__ == '__main__':
+    pytest.main(['ShopManagement_ShopManagement.py'])
