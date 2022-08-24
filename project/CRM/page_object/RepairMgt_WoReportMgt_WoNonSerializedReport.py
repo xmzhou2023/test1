8fec8c77127d00afd5ac33140c71f36816daf3e7
wenminxiong <xwm101827>
2022-08-24 18:04:56 +0800

    提交crm

---
+++ project/CRM/page_object/RepairMgt_WoReportMgt_WoNonSerializedReport.py
@@ -0,0 +1,73 @@
+import allure, os
+from public.base.basics import Base, sleep
+from libs.common.read_element import Element
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import logging
+from ..test_case.conftest import *
+from datetime import datetime
+
+object_name = os.path.basename(__file__).split('.')[0]
+user = Element(pro_name, object_name)
+
+class WONonSerializedReport(Base):
+    """非序列化工单报表"""
+    given_data = datetime.today().date()
+    logging.info('今天的日期: {} '.format(given_data))
+    first_day_of_month = given_data.replace(day=1)
+    logging.info('本月的第一天是:{}'.format(first_day_of_month))
+
+    @allure.step("非序列化工单报表查询")
+    def search_NonwoserlistReport(self, scope):
+        self.refresh()
+        sleep(1)
+        self.is_click(user['WO Non Serialized Date开始日期搜索框'])
+        self.hover(user['WO Non Serialized Date开始日期搜索框'])
+        self.is_click(user['清除时间搜索框'])
+        if scope == all:
+            # 查询非序列化工单报表所有数据
+            self.is_click(user['WO NonSerialized Report Search'])
+        else:
+            # 查询非序列化工单报表当月SL国家的数据
+            self.is_click(user['WO Non Serialized Date开始日期搜索框'])
+            self.input_text(user['WO Non Serialized Date开始日期搜索框'], txt=str(WONonSerializedReport.first_day_of_month))
+            self.is_click(user["Country搜索框"])
+            self.hover(user['Country下拉选择'], choice='SL')
+            self.is_click(user["Country下拉选择"])
+            # self.is_click(user['序列化Search按钮'])
+            self.is_click(user['WO NonSerialized Report Search'])
+
+    @allure.step("查询非序列化工单报表数量")
+    def search_stock(self, stock):
+        sql = SQL('CRM', 'test')
+        if stock == all:  # 查询非序列化工单所有数据
+            record = sql.query_db('SELECT count(*) FROM crm_rc_deserialize_work_order dwo LEFT JOIN crm_rc_deserialize_repair_result drr ON dwo.id = drr.work_order_id AND drr.is_deleted = 0 LEFT JOIN crm_rc_deserialize_repair_result_detail drrd ON drr.id = drrd.result_id AND drrd.is_deleted = 0 WHERE dwo.is_deleted = 0')
+
+        else:  # 有条件的查询非序列化工单数据
+            record = sql.query_db("SELECT count(*) FROM crm_rc_deserialize_work_order dwo LEFT JOIN crm_rc_deserialize_repair_result drr ON dwo.id = drr.work_order_id AND drr.is_deleted = 0 LEFT JOIN crm_rc_deserialize_repair_result_detail drrd ON drr.id = drrd.result_id AND drrd.is_deleted = 0 WHERE dwo.is_deleted = 0 and dwo.creation_time>='{}' and dwo.warehouse_country_code='SL'".format(WONonSerializedReport.first_day_of_month))
+
+        dict_record = record[0]
+        print(dict_record)
+        record_value = dict_record['count(*)']
+        logging.info('数据库查询到的非序列化工单报表数据为:{}'.format(record_value))
+        search_num = self.get_element_attribute(user['报表总数'], 'textContent')
+        num = ''.join(filter(str.isdigit, search_num))
+        num = int(num)
+        logging.info('非序列化工单报表查询页查到的数量:{}'.format(num))
+
+        return record_value, num
+
+
+
+
+    @allure.step("报表导出")
+    def download_report(self):
+        self.is_click(user['WO NonSerialized Report Export'], 'Export')
+        # self.alert_ok()
+        self.is_click(user['确认导出'], 'OK')
+        # logging.info("输入框键入{}".format(content))
+        sleep(1)
+
+
+if __name__ == '__main__':
+    pass
---
+++ project/CRM/test_case/RepairMgt_WOSerializedMgt_WOSerializedList.py
@@ -0,0 +1,88 @@
+import allure
+import pytest
+from public.base.assert_ui import *
+from project.CRM.page_object.Center_Component import NavPage
+from project.CRM.page_object.RepairMgt_WOSerializedMgt_WOSerializedList import *
+"""
+    用例等级说明:
+        blocker级别:中断缺陷(客户端程序无响应，无法执行下一步操作)
+        critical级别: 临界缺陷(功能点缺失)
+        normal级别:普通缺陷(数值计算错误)
+        minor级别: 次要缺陷(界面错误与UI需求不符)
+        trivial级别:轻微缺陷(必输项无提示， 或者提示不规范)
+"""
+#
+@pytest.fixture(scope='module',autouse=True)
+def class_setup_fixture(drivers):
+    num = NavPage(drivers)
+    num.click_gotonav("WMS", "Stock In/Out Mgt", "Initialize Inventory")
+    num = DomAssert(drivers)
+    num.assert_url("/wms/stockInOutMgt/initializeInventory")
+    logging.info("前置条件:添加物料库存")
+    num = WOSerializedListAdd(drivers)
+    num.add_material()
+
+
+
+@pytest.fixture(scope='module',autouse=True)
+def module_setup_fixture(drivers):
+    logging.info("前置条件:进入序列化工单列表页")
+    user = NavPage(drivers)
+    user.click_gotonav("Repair Mgt", "WO Serialized Mgt", 'WO Serialized List')
+    user = DomAssert(drivers)
+    user.assert_url("/maintenanceMgt/workorderSerializedMgt/workorderSerializedList")
+
+@allure.feature("WO Serialized Mgt-WO Serialized List")
+class TestAddWoList:
+    @allure.story("新增序列化工单成功") # 场景名称
+    @allure.title("序列化工单新增成功")  # 用例名称
+    @allure.description("输入的imei在所选的仓库中有defective状态库存")
+    @allure.severity("blocker")  # 用例等级
+    @pytest.mark.smoke # 用例标记
+    def test_001_001(self, drivers):   # 用例名称取名规范'test+场景编号+用例编号'
+        sn_imei = WOSerializedListAdd.search_imei_stock(self)
+        num = WOSerializedListAdd(drivers)
+        num.add_woserlist(Warehouse='API_母仓', imei=sn_imei)
+        num = DomAssert(drivers)
+        num.assert_att(sn_imei)
+
+    @allure.story("新增序列化工单失败")  # 场景名称
+    @allure.title("序列化工单新增失败")  # 用例名称
+    @allure.description("输入的imei不在当前所选仓库")
+    @allure.severity("blocker")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_001_002(self, drivers):  # 用例名称取名规范'test+场景编号+用例编号'
+        sn_imei = WOSerializedListAdd.add_imei(self)
+        num = WOSerializedListAdd(drivers)
+        num.add_woserlist(Warehouse='API_母仓', imei=sn_imei)
+        num = DomAssert(drivers)
+        num.assert_att("IMEI/SN Does not exist or does not belong to the current location！")
+
+@allure.feature("WO Serialized Mgt-WO Serialized List")
+class TestSearchWoList:
+    @allure.story("查询序列化工单所有数据")  # 场景名称
+    @allure.title("查询序列化工单所有数据")  # 用例名称
+    @allure.description("查询条件都设置为空,查询所有序列化工单数据")
+    @allure.severity("blocker")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_002_001(self, drivers):  # 用例名称取名规范'test+场景编号+用例编号'
+        num = WOSerializedListSearch(drivers)
+        num.search_woserlist(scope=all)
+        record = num.search_stock(stock=all)
+        ValueAssert.value_assert_equal(record[0], record[1],)
+
+
+    @allure.story("查询序列化工单部分数据")  # 场景名称
+    @allure.title("查询印度当月的序列化工单数据")  # 用例名称
+    @allure.description("查询条件设置时间为当月，国家选择印度进行查询")
+    @allure.severity("blocker")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_002_002(self, drivers):  # 用例名称取名规范'test+场景编号+用例编号'
+        num = WOSerializedListSearch(drivers)
+        num.search_woserlist(scope='part')
+        record = num.search_stock(stock='part')
+        ValueAssert.value_assert_equal(record[0], record[1],)
+
+if __name__ == '__main__':
+    pass
+
