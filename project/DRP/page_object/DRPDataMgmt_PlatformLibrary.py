7b14dc06df603210f2928cb8f63a4de7c8f8b52a
longjiang <jiang.long@transsion.com>
2022-08-26 16:36:32 +0800

    提交DRP数据管理-平台库代码

--- project/DRP/page_object/DRPDataMgmt_PlatformLibrary.py
+++ project/DRP/page_object/DRPDataMgmt_PlatformLibrary.py
@@ -13,29 +13,53 @@ user = Element(pro_name, object_name)
 class PlatformLibrary(Base):
     """平台库"""
 
-    @allure.step("展开平台、物料状态、可用状态 下拉框")
-    def screenCondition(self, condition):
+    @allure.step("选择指定选项")
+    def screenOption(self, condition,option):
         self.is_click(user['查询条件'],condition)
-        logging.info("查询项:{}".format(condition))
+        self.is_click(user['下拉选项'], option)
+        logging.info("系列：{}".format(option))
 
-    @allure.step("选择指定选项")
-    def screenOption(self, condition, option):
-        pullDown = self.find_elements(user['下拉列表'])
-        optionList = []
-        for i in range(len(pullDown)):
-            optionList.append(pullDown[i].text)
-        if option in optionList:
-            self.is_click(user['输入框下拉'], option)
-        elif option not in optionList:
-            self.is_click(user['查询条件'], condition)
-        else:
-            self.is_click(user['筛选框断言'])
-        logging.info("查询选项：{}".format(option))
-
-    @allure.step("选择指定选项")
+    @allure.step("点击查询按钮")
     def queryButton(self):
         self.is_click(user['查询按钮'])
         logging.info("点击查询按钮")
+
+    @allure.step("查询结果断言")
+    def screenAssert(self,condition, value):
+        if condition == "平台":
+            assertvalue = self.element_text(user['平台列表'],"3")
+            ValueAssert.value_assert_equal(assertvalue,value)
+        if condition == "物料状态":
+            assertvalue = self.element_text(user['平台列表'], "4")
+            ValueAssert.value_assert_equal(assertvalue,value)
+        if condition == "可用状态":
+            assertvalue = self.element_text(user['平台列表'], "1")
+            ValueAssert.value_assert_equal(assertvalue,value)
+
+    @allure.step("查询结果断言")
+    def exceptionAssert(self):
+        value = self.element_text(user['暂无数据(断言)'])
+        ValueAssert.value_assert_equal(value,"暂无数据")
+
+    @allure.step("前置条件 - 按照条件过滤平台库")
+    def precondition(self,drivers):
+        platform = PlatformLibrary(drivers)
+        platform.screenOption("平台","HK-整机")
+        platform.screenOption("物料状态","整机")
+        platform.screenOption("可用状态","启用")
+        platform.queryButton()
+
+    @allure.step("点击重置按钮")
+    def resetButton(self):
+        self.is_click(user['重置按钮'])
+        logging.info("点击重置按钮")
+
+    @allure.step("列表数据断言")
+    def listAssert(self):
+        return self.element_text(user['数据条数（断言）'])
+
+
+
 
 
 
--- project/DRP/test_case/DRPDataMgmt_PlatformLibrary.py
+++ project/DRP/test_case/DRPDataMgmt_PlatformLibrary.py
@@ -19,14 +19,77 @@ def setup_module(drivers):
 class TestSearchPlatform:
     @allure.story("查询平台")
     @allure.title("按照平台条件过滤平台库信息")
-    @allure.description("选择平台选项，查询 平台=HK-CKD 的平台库信息")
+    @allure.description("选择平台选项，查询 平台=埃塞转出口 的平台库信息")
     @allure.severity("blocker")  # blocker\critical\normal\minor\trivial
     @pytest.mark.smoke
     def test_001_001(self, drivers):
         platform = PlatformLibrary(drivers)
-        platform.screenCondition("平台")
-        platform.screenOption("平台", "HK-CKD")
+        platform.screenOption("平台","埃塞转出口")
         platform.queryButton()
+        platform.screenAssert("平台","埃塞转出口")
+
+    @allure.story("查询物料状态")
+    @allure.title("按照物料状态 条件过滤平台库信息")
+    @allure.description("选择物料状态选项，查询 物料状态=整机 的平台库信息")
+    @allure.severity("blocker")  # blocker\critical\normal\minor\trivial
+    @pytest.mark.smoke
+    def test_001_002(self, drivers):
+        platform = PlatformLibrary(drivers)
+        platform.screenOption("物料状态","整机")
+        platform.queryButton()
+        platform.screenAssert("物料状态","整机")
+
+    @allure.story("查询可用状态")
+    @allure.title("按照可用状态 条件过滤平台库信息")
+    @allure.description("选择可用状态选项，查询 可用状态=启用 的平台库信息")
+    @allure.severity("blocker")  # blocker\critical\normal\minor\trivial
+    @pytest.mark.smoke
+    def test_001_003(self, drivers):
+        platform = PlatformLibrary(drivers)
+        platform.screenOption("可用状态","启用")
+        platform.queryButton()
+        platform.screenAssert("可用状态","启用")
+
+
+    @allure.story("组合条件查询 平台、物料状态、可用状态")
+    @allure.title("按照组合条件过滤平台库信息")
+    @allure.description("选择可用状态选项，查询 平台=HK-整机 物料状态=整机 可用状态=启用 的平台库信息")
+    @allure.severity("blocker")  # blocker\critical\normal\minor\trivial
+    @pytest.mark.smoke
+    def test_001_004(self, drivers):
+        platform = PlatformLibrary(drivers)
+        platform.screenOption("平台","HK-整机")
+        platform.screenOption("物料状态","整机")
+        platform.screenOption("可用状态","启用")
+        platform.queryButton()
+        platform.screenAssert("平台","HK-整机")
+
+    @allure.story("异常 查询结果为空")
+    @allure.title("按照组合条件过滤平台库信息，查询结果无数据")
+    @allure.description("选择可用状态选项，查询 平台=HK-整机 物料状态=CKD 平台库信息，查询结果无数据展示")
+    @allure.severity("normal")  # blocker\critical\normal\minor\trivial
+    @pytest.mark.smoke
+    def test_001_005(self, drivers):
+        platform = PlatformLibrary(drivers)
+        platform.screenOption("平台","HK-整机")
+        platform.screenOption("物料状态","CKD")
+        platform.queryButton()
+        platform.exceptionAssert()
+
+    @allure.story("查询重置")
+    @allure.title("先按条件查询平台信息，点击重置按钮，清空查询条件")
+    @allure.description("先按条件查询平台信息，点击重置按钮，清空查询条件，列表展示全部数据")
+    @allure.severity("normal")  # blocker\critical\normal\minor\trivial
+    @pytest.mark.smoke
+    def test_001_006(self, drivers):
+        platform = PlatformLibrary(drivers)
+        platform.precondition(drivers)
+        before = platform.listAssert()
+        platform.resetButton()
+        after = platform.listAssert()
+        ValueAssert.value_assert_Notequal(before, after)
+
+
 
 
 
