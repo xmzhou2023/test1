bbba413f03d5f88eb16c9d1010abfd54222abd47
jieru.zhao <jieru.zhao@transsion.com>
2022-08-30 17:28:29 +0800

    commit:TLC_web项目代码提交 author:zhaojieru
    # Conflicts:
    #	public/data/datasource/account.csv

---
+++ project/TLC_web/test_case/My_Files.py
@@ -0,0 +1,7 @@
+import allure
+import pytest
+from public.base.assert_ui import *
+from project.TLC_web.page_object.Center_Component import NavPage
+from project.TLC_web.page_object.My_Files import document
+
+
