181c31b1adeb8a4606c3c9091ddf044c40f39460
daimei.shi <daimei.shi@transsion>
2022-08-30 13:53:53 +0800

    修改srm文件内容：修改供应商绩效考核的用例

--- project/SRM/page_object/PerformanceAppraisal_SupplyCategory.py
+++ project/SRM/page_object/PerformanceAppraisal_SupplyCategory.py
@@ -14,9 +14,6 @@ app= Element(pro_name, object_name)
 
 
 
-
-
-
 class Performance(Base):
     """供应商绩效类"""
     @allure.step("进入供应商绩效考核")
@@ -43,17 +40,24 @@ class Performance(Base):
         return self.find_element(app["评估代码供货品类配置-新建弹窗标题"]).text
 
     def creat_SupplyCategory_close(self):
+        # self.frame_enter(app["评估代码供货品类配置页面内容iframe"])
+        # self.is_click(app["评估代码供货品类配置-新建"])
+        # time.sleep(2)
         self.is_click(app["评估代码供货品类配置-新建弹窗-关闭"])
+        self.frame_back()
+
 
     def creat_SupplyCategory_cancel(self):
-        self.is_click(app["评估代码供货品类配置-新建"])
+        # self.frame_enter(app["评估代码供货品类配置页面内容iframe"])
+        # self.is_click(app["评估代码供货品类配置-新建"])
         self.is_click(app["评估代码供货品类配置-新建-取消"])
-
-
+        self.frame_back()
 
     def creat_select_code(self,code):
+        self.frame_enter(app["评估代码供货品类配置页面内容iframe"])
         self.is_click(app["评估代码供货品类配置-新建"])
         self.input_text(app['评估代码供货品类配置-新建-评估代码'],txt=code)
+
 
     def creat_select_material(self):
         self.is_click(app['评估代码供货品类配置-新建-小类搜索'])
@@ -62,7 +66,6 @@ class Performance(Base):
         self.is_click(app['评估代码供货品类配置-新建-小类选择'])
         self.is_click(app['评估代码供货品类配置-新建-小类-确定'])
         self.frame_back()
-
 
 
     def creat_select_rule(self):
@@ -74,7 +77,6 @@ class Performance(Base):
         self.is_click(app["评估代码供货品类配置-新建-评分规则-确定"])
 
 
-
     def creat_SelectOK(self):
         self.frame_back()
         self.frame_enter(app["评估代码供货品类配置页面内容iframe"])
@@ -82,17 +84,18 @@ class Performance(Base):
         self.frame_back()
         self.is_click(app["评估代码供货品类配置-新建-确定-保存确定"])
 
+
     @allure.step("获取新建成功弹窗提示内容")
     def get_alert_text(self):
         # 获取弹窗内容，srm使用
         return self.find_element(app["评估代码供货品类配置-新建-操作成功提示"]).text
 
 
-
     def search_code(self, code):
         self.frame_enter(app["评估代码供货品类配置页面内容iframe"])
         self.input_text(app["评估代码供货品类配置-评估代码查询输入框"], code)
         self.is_click(app["评估代码供货品类配置-查询"])
+        self.frame_back()
         # time.sleep(3)
         # self.find_elements_choice('//button[text()=" 查询"]',1)
 
@@ -101,13 +104,9 @@ class Performance(Base):
         return self.find_element(app["评估代码搜索结果"]).text
 
 
-
-
-
     def search_material(self, material):
         self.input_text(app["评估代码供货品类配置-物料小类查询输入框"], material)
         self.is_click(app["评估代码供货品类配置-查询"])
-    #
 
 
     @allure.step("最小化供应商绩效考核窗口")
@@ -118,8 +117,8 @@ class Performance(Base):
 
 
 
-
-
-
 if __name__ == '__main__':
     pass
+
+
+
