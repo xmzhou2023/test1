046eaa5e9768b0bcd713e075c52be7774589d3bd
jieru.zhao <jieru.zhao@transsion.com>
2022-09-01 17:52:32 +0800

    commit：TLC删除多余文件夹 author：zhaojieru

--- project/TLC_web/test_case/My_Files.py
+++
@@ -1,7 +0,0 @@
-import allure
-import pytest
-from public.base.assert_ui import *
-from project.TLC_web.page_object.Center_Component import NavPage
-from project.TLC_web.page_object.My_Files import document
-
-
