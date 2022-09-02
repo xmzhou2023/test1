e352100f91c1ab09b74c6deb0262a8a5b5d806ce
Evan <yong.liu7@transsion.com>
2022-09-02 21:00:31 +0800

    修改元素库自检程序为批量提示

--- libs/common/inspect_ymal.py
+++ libs/common/inspect_ymal.py
@@ -6,6 +6,7 @@ import yaml
 from libs.common.time_ui import timestamp
 from libs.config.conf import PEROJECT_PATH, LOCATE_MODE
 
+
 def get_FolderName(filepath):
     pro_list = []
     pathDir = os.listdir(filepath)
@@ -14,9 +15,10 @@ def get_FolderName(filepath):
         pro_list.append(child)
     return pro_list
 
+
 def inspect_element():
     """审查所有的元素是否正确"""
-    print("【开始】请稍候，元素定位库文件自检程序开始！" )
+    print("【开始】请稍候，元素定位库文件自检程序开始！")
     start_time = timestamp()
     pro_list = get_FolderName(PEROJECT_PATH)
 
@@ -30,24 +32,34 @@ def inspect_element():
                     data = yaml.safe_load(f)
                     try:
                         if data is None:
-                            error_list.append('【%s】项目【%s】模块格式不符' % (pro, i))
+                            error_list.append('【项目】%s【模块】%s中元素格式不符' % (pro, i))
 
-                        for k in data.values():
+                        if isinstance(data, dict) is False:
+                            error_list.append(
+                                '【项目】%s【模块】%s请检查元素名和表达式固定格式【元素定位名: 定位方式==定位路径】' % (pro, i))
+
+                        for k,v in data.items():
+
+                            if k is None:
+                                error_list.append('【项目】%s【模块】%s【元素】%s【定位表达式】%s定位方式和值未填写' % (pro, i, k, v))
+
                             if '==' not in k:
-                                error_list.append('【%s】项目【%s】模块【%s】元素xpath格式不符' % (pro, i, k))
+                                error_list.append('【项目】%s【模块】%s【元素】%s【定位表达式】%s标签和路径需用==间隔' % (pro, i, k, v))
 
                             pattern, value = k.split('==')
 
-
                             if pattern not in LOCATE_MODE:
-                                error_list.append('【%s】项目【%s】模块【%s】元素没有指定类型' % (pro, i, k))
+                                error_list.append('【项目】%s【模块】%s【元素】%s【定位表达式】%s没有指定类型' % (pro, i, k, v))
 
                             if pattern in ('id', 'name', 'css', 'class', 'text', 'partial-link', 'tag'):
                                 continue
 
                             if pattern == 'xpath':
                                 if '/' not in value:
-                                    error_list.append('【%s】项目【%s】模块【%s】元素xpath类型与值不配！' % (pro, i, k))
+                                    error_list.append('【项目】%s【模块】%s【元素】%s【定位表达式】%sxpath类型与值不配！' % (pro, i, k, v))
+
+                                if '“' in value or '‘' in value:
+                                    error_list.append('【项目】%s【模块】%s【元素】%s【定位表达式】%s注意不要用中文符号的引号！' % (pro, i, k, v))
 
                             # if pattern == 'css':
                             #     if '/' not in value:
@@ -61,11 +73,11 @@ def inspect_element():
                     except:
                         continue
 
-
     end_time = timestamp()
     print("【结束】恭喜您，元素库文件自检程序完成！用时%.3f秒！" % (end_time - start_time))
     if len(error_list) != 0:
         raise AttributeError('\n%s\n【提示】请尽快检查并修改自己的用例，如有异议，请尽快联系我们，谢谢！' % error_list)
+
 
 if __name__ == '__main__':
     # print(get_FolderName(PEROJECT_PATH))
