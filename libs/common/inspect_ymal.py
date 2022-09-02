deff2c17078ca5595f6f0a6fafb641297b48b916
Evan <yong.liu7@transsion.com>
2022-09-02 17:00:12 +0800

    修改元素库自检程序为批量提示

--- libs/common/inspect_ymal.py
+++ libs/common/inspect_ymal.py
@@ -20,7 +20,7 @@ def inspect_element():
     start_time = timestamp()
     pro_list = get_FolderName(PEROJECT_PATH)
 
-    error_list = 0
+    error_list = []
     for pro in pro_list:
         ELEMENT_PATH = os.path.join(PEROJECT_PATH, pro, 'page_element')
         for i in os.listdir(ELEMENT_PATH):
@@ -29,43 +29,43 @@ def inspect_element():
                 with open(_path, encoding='utf-8') as f:
                     data = yaml.safe_load(f)
                     try:
-                        if data.values is None:
-                            print('【提示】【%s】项目【%s】模块格式不符，亲再检查下格式哦！' % (pro, i))
-                            error_list += 1
+                        if data is None:
+                            error_list.append('【%s】项目【%s】模块格式不符' % (pro, i))
 
                         for k in data.values():
                             if '==' not in k:
-                                print('【提示】【%s】项目【%s】模块【%s】元素xpath格式不符，亲再检查下格式哦！' % (pro, i, k))
-                                error_list += 1
+                                error_list.append('【%s】项目【%s】模块【%s】元素xpath格式不符' % (pro, i, k))
 
                             pattern, value = k.split('==')
 
+
                             if pattern not in LOCATE_MODE:
-                                print('【提示】【%s】项目【%s】模块【%s】元素没有指定类型，亲再检查下格式哦！' % (pro, i, k))
-                                error_list += 1
+                                error_list.append('【%s】项目【%s】模块【%s】元素没有指定类型' % (pro, i, k))
+
+                            if pattern in ('id', 'name', 'css', 'class', 'text', 'partial-link', 'tag'):
+                                continue
+
                             if pattern == 'xpath':
                                 if '/' not in value:
-                                    print('【提示】【%s】项目【%s】模块【%s】元素xpath类型与值不配，亲再检查下格式哦！' % (pro, i, k))
-                                    error_list += 1
-                            if pattern == 'css':
-                                if '/' not in value:
-                                    print('【提示】【%s】项目【%s】模块【%s】元素css类型与值不配' % (pro, i, k))
-                                    error_list += 1
-                            if pattern == 'id':
-                                if '/' not in value:
-                                    print('【提示】【%s】项目【%s】模块【%s】元素css类型与值不配' % (pro, i, k))
-                                    error_list += 1
-                            if pattern in ('id', 'name', 'css', 'class', 'text', 'partial-link', 'tag'):
-                                print('【提示】【%s】项目【%s】模块【%s】元素类型与值不匹配，亲再检查下格式哦！' % (pro, i, k))
-                                error_list += 1
+                                    error_list.append('【%s】项目【%s】模块【%s】元素xpath类型与值不配！' % (pro, i, k))
+
+                            # if pattern == 'css':
+                            #     if '/' not in value:
+                            #         print('【提示】【%s】项目【%s】模块【%s】元素css类型与值不配' % (pro, i, k))
+                            #         error_list += 1
+
+                            # if pattern == 'id':
+                            #     if '/' not in value:
+                            #         print('【提示】【%s】项目【%s】模块【%s】元素css类型与值不配' % (pro, i, k))
+                            #         error_list += 1
                     except:
-                        error_list += 1
+                        continue
 
 
     end_time = timestamp()
     print("【结束】恭喜您，元素库文件自检程序完成！用时%.3f秒！" % (end_time - start_time))
-    if error_list != 0:
-        raise AttributeError('【提示】请尽快检查并修改自己的用例，如有异议，请尽快联系我们，谢谢！')
+    if len(error_list) != 0:
+        raise AttributeError('\n%s\n【提示】请尽快检查并修改自己的用例，如有异议，请尽快联系我们，谢谢！' % error_list)
 
 if __name__ == '__main__':
     # print(get_FolderName(PEROJECT_PATH))
