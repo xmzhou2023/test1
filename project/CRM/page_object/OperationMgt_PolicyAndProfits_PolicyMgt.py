e0c7b7571e05476eb8b964178ef6abd2f19fe690
yue.xu <yue.xu@transsion.com>
2022-09-02 15:22:29 +0800

    修改crm项目冲突-删除无用文件

--- project/CRM/page_object/OperationMgt_PolicyAndProfits_PolicyMgt.py
+++ project/CRM/page_object/OperationMgt_PolicyAndProfits_PolicyMgt.py
@@ -18,21 +18,25 @@ class PolicyPage(Base):
         if country is not None:
             self.is_click(user["筛选框"],choice="country")
             self.input_text(user["筛选框"],choice="country",txt=country)
+            logging.info("国家筛选框输入{}".format(country))
             sleep(0.5)
             self.is_click(user["下拉筛选框_第一条数据"])
         if brand is not None:
             self.is_click(user["筛选框"],choice="brandCategory")
             self.input_text(user["筛选框"],choice="brandCategory", txt=brand)
+            logging.info("品牌品类筛选框输入{}".format(brand))
             sleep(0.5)
             self.is_click(user["下拉筛选框_第二条数据"])
         if project is not None:
             self.is_click(user["筛选框"],choice="projectName")
             self.input_text(user["筛选框"],choice="projectName", txt=project)
+            logging.info("机型筛选框输入{}".format(project))
             sleep(0.5)
             self.is_click(user["下拉筛选框_第一条数据"])
         if keyword is not None:
             self.is_click(user["筛选框"], choice="keyword")
             self.input_text(user["筛选框"], choice="keyword", txt=keyword)
+            logging.info("keyword筛选框输入{}".format(keyword))
             sleep(0.5)
         self.is_click(user["搜索按钮"])
         sleep(0.5)
@@ -41,6 +45,7 @@ class PolicyPage(Base):
     def get_total(self):
         sleep(0.5)
         total = self.element_text(user["total数量"])
+        logging.info("获取total文本{}".format(total))
         num = total.split(" ",1)
         number = num[1]
         return number
