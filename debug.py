c4a0658537bf8ce5668ad360954b478d1100ee8b
longjiang <jiang.long@transsion.com>
2022-09-08 15:27:43 +0800

    提交月度供应资源 代码

--- debug.py
+++ debug.py
@@ -1,6 +1,37 @@
 from project.TBM.api.api import APIRequest
 import requests
 
+# cd C:\Program Files\Google\Chrome\Application
+# chrome.exe --remote-debugging-port=9527 --user-data-dir="D:\selenium\AutomationProfile"
+# http://10.250.112.166:9000/#/systemManage/userManage
+# http://39.101.161.151/#/popupwindow
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+# from page_base.webpage import WebPage
+
+options = Options()
+options.add_experimental_option("debuggerAddress", "127.0.0.1:9527")
+driver = webdriver.Chrome(options=options)
+
+
+import re
+# # driver.find_element(By.XPATH,"//*[@id='app']/div/div[2]/div[2]/section/div/div/div[2]/div[4]/div[2]/table/tbody/tr/td[8]/div/div/button[1]/span").click()
+a = driver.find_element(By.XPATH,"//div[@class='el-table__fixed-body-wrapper']//table[@class='el-table__body']//tr//td[2]").get_attribute("innerHTML")
+c = driver.find_element(By.XPATH,"//div[@class='el-table__fixed-body-wrapper']//table[@class='el-table__body']//tr[2]//td[2]").get_attribute("innerHTML")
+b = driver.find_element(By.XPATH,"//div[@class='el-table__fixed-body-wrapper']//table[@class='el-table__body']//tr//td[2]")
+print(b.text)
+print(re.findall("<div>(.*?)</div>", a)[0])
+print(re.findall("<div>(.*?)</div>", c))
+
+
+txt = '#*RD -015.92 +0000.C\r'
+print(float(re.findall("#*RD(.*?)\+0000", txt)[0]))
+
+
 # token = APIRequest().tbm_login()
 # # body1 = {"status": "Process"}
 # headers = {'Content-Type': 'application/json',
@@ -30,16 +61,16 @@ import requests
 # chehui = requests.delete(url=f'https://pfgatewayidct.transsion.com:9188/process-center/process-center/instance/93fcdc48-1498-11ed-bee3-0242ac130002/revoke',
 #                          headers=headers).json()
 # print(chehui)
-login_body = {"username":"18645960", "pwd":"eExpbHk2eA=="}
-token = requests.post(url='http://pfgatewayuat.transsion.com:9099/service-pt-unified-auth/pt-unified-auth/login',
-                  json=login_body).json()['data']['token']
-headers = {'Content-Type': 'application/json',
-           'Authorization': token}
-list_body = {"current":1,"size":1,"param":{"code": "20220811025552978103"}}
-b = requests.post(url='https://pfgatewayuat.transsion.com:9199/service-hulk-workflow-transfer/workflow/task/applyList',
-                  json=list_body, headers=headers).json()['data']['list'][0]['applicant']
-# print(b)
-d='李小素'
-c = requests.post(url='https://pfgateway.transsion.com:9199/service-cp-uc-extend/cp-uc-extend/Department/queryDeptAndEmployee',
-                  data=d.encode('utf-8'), headers=headers).json()
-print(c)
+# login_body = {"username":"18645960", "pwd":"eExpbHk2eA=="}
+# token = requests.post(url='http://pfgatewayuat.transsion.com:9099/service-pt-unified-auth/pt-unified-auth/login',
+#                   json=login_body).json()['data']['token']
+# headers = {'Content-Type': 'application/json',
+#            'Authorization': token}
+# list_body = {"current":1,"size":1,"param":{"code": "20220811025552978103"}}
+# b = requests.post(url='https://pfgatewayuat.transsion.com:9199/service-hulk-workflow-transfer/workflow/task/applyList',
+#                   json=list_body, headers=headers).json()['data']['list'][0]['applicant']
+# # print(b)
+# d='李小素'
+# c = requests.post(url='https://pfgateway.transsion.com:9199/service-cp-uc-extend/cp-uc-extend/Department/queryDeptAndEmployee',
+#                   data=d.encode('utf-8'), headers=headers).json()
+# print(c)
