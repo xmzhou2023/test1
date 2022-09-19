bb747d2214a9aca7aa91c225b39c32a175da5d8d
jieru.zhao <jieru.zhao@transsion.com>
2022-09-19 15:00:35 +0800

    commit:TLC 我的文件新增用例 author:zhaojieru

--- project/TLC/page_object/My_files_delete.py
+++ project/TLC/page_object/My_files_delete.py
@@ -9,24 +9,9 @@ from ..test_case.conftest import *
 object_name = os.path.basename(__file__).split('.')[0]
 user = Element(pro_name, object_name)
 
-# class UserPage(Base):
-#     """用户类"""
-#
-#     @allure.step("查找工号")
-#     def search_user(self, jobnum=None,name=None):
-#         if jobnum is not None:
-#             self.readonly_input_text(user['用户管理-工号输入框'], txt=jobnum)
-#             sleep(2)
-#             self.is_click(user['用户管理-工号下拉列表'], jobnum)
-#         if name is not None:
-#             self.readonly_input_text(user['用户管理-姓名输入框'], txt=name)
-#             sleep(2)
-#             self.is_click(user['用户管理-姓名下拉列表'], name)
-#         self.is_click(user['用户管理-查询'])
-#         sleep()
+class UserPage(Base):
+    """用户类"""
 
-
-class Tool(Base):
     @allure.step('导航菜单')
     def click_menu(self, fType, sType):
         self.mouse_hover(user['一级菜单'], fType)
@@ -47,9 +32,6 @@ class Tool(Base):
     def input(self, ymal, txt, choice=None):
         self.input_text(user[ymal], txt, choice)
 
-    @allure.step('跳转窗口')
-    def handle_switch_window(self, n):
-        self.switch_window(n)
 
 
 if __name__ == '__main__':
