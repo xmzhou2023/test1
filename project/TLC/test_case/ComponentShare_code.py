99c9c939ab76809b00711ef5fbbceb50b840880d
jieru.zhao <jieru.zhao@transsion.com>
2022-09-29 10:25:19 +0800

    commit:更换为UAT环境 author：zhaojieru

--- project/TLC/test_case/ComponentShare_code.py
+++ project/TLC/test_case/ComponentShare_code.py
@@ -1,6 +1,6 @@
 import allure
 import pytest
-from project.TLC.page_object.ComponentSharing import Share
+from project.TLC.page_object.ComponentShare_code import Share
 from public.base.assert_ui import DomAssert
 
 """
@@ -83,5 +83,41 @@ class TestComponentSharing:
         tools.click('删除确认')
         DomAssert(drivers).assert_page_source('auto_testing_add_components_pub_001')
         pass
+
+    @allure.story("组件code校验")  # 场景名称
+    @allure.title("组件code查看与复制")  # 用例名称
+    @allure.description("新增组件，复制code")
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_002_001(self, drivers):  # 用例名称取名规范'test+场景编号+用例编号'
+        tools = Share(drivers)
+        tools.click_menu('组件中心', '我的文件')
+        tools.click('空间', '我的空间')
+        tools.hover('新增')
+        tools.click('新增组件')
+        tools.input('新增输入框', 'auto_testing_add_components_pri_001', 'code')
+        tools.input('新增输入框', 'auto_testing_add_components_pri_001', 'name')
+        tools.click('新增对话框按钮', 'confirm')
+        tools.switch_window(1)
+        # 查看code
+        tools.hover('编码查看')
+        DomAssert(drivers).assert_att('auto_testing_add_components_pri_001')
+        tools.close_switch(1)
+        # 复制code
+        tools.click_menu('组件中心', '我的文件')
+        tools.click('空间', '我的空间')
+        tools.hover('组件Item')
+        tools.click('组件Item more')
+        tools.click('复制编码', 'auto_testing_add_components_pri_001')
+        DomAssert(drivers).assert_att('复制成功')
+        # 删除
+        tools.click_menu('组件中心', '我的文件')
+        tools.click('空间', '我的空间')
+        tools.hover('组件Item')
+        tools.click('组件Item more')
+        tools.click('删除', 'auto_testing_add_components_pri_001')
+        tools.click('删除确认')
+        DomAssert(drivers).assert_page_source('auto_testing_add_components_pri_001')
+        pass
 if __name__ == '__main__':
-    pytest.main(['project/TLC/test_case/ComponentSharing.py'])
+    pytest.main(['project/TLC/test_case/ComponentShare_code.py'])
