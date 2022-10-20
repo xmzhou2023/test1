3c3f8d3b7413cf6d22778170f191f4458bda04c5
wenminxiong <xwm101827>
2022-10-20 16:06:28 +0800

    crm修改用例

--- public/libs/unified_login/page_object/login.py
+++ public/libs/unified_login/page_object/login.py
@@ -61,6 +61,17 @@ class LoginPage(Base):
         imgcode = self.get_graphical_code(login['图形验证码'])
         self.input_text(login['图形验证码输入框'], imgcode)
 
+    def input_verify_code(self):
+        """输入验证码，生产环境用 2022-9-30,熊文敏"""
+        verify_code = self.get_graphical_code(login["验证码图标"])
+        self.is_click(login['验证码输入框'])
+        self.input_text(login["验证码输入框"], verify_code)
+        sleep(2)
+
+    def code_clear(self):
+        self.clear_code(login["验证码输入框"])
+        sleep(0.5)
+
 
 """DCR登录类"""
 class DcrLoginPage(Base):
