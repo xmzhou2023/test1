d9f9e67139860c87af76f6bb88c54a9512291435
18648901 <hongmei.li@transsion.com>
2022-10-20 15:04:01 +0800

    修改生产环境的xpath绝对定位，改为相对定位

--- project/DCR_GLOBAL/page_object/PurchaseManagement_InboundReceipt.py
+++ project/DCR_GLOBAL/page_object/PurchaseManagement_InboundReceipt.py
@@ -42,27 +42,6 @@ class InboundReceiptPage(Base):
         Base.presence_sleep_dcr(self, user['收货第一个复选框'])
         self.is_click_dcr(user['收货第一个复选框'])
 
-    def click_quick_received(self):
-        """快速收货页面，点击Quick Received按钮"""
-        self.is_click_dcr(user['快速收货按钮'])
-        sleep(2)
-
-    def click_save(self):
-        """快速收货页面，点击Save按钮"""
-        Base.presence_sleep_dcr(self, user['保存'])
-        self.is_click(user['保存'])
-
-    def get_successfully_text(self):
-        """快速收货页面，提交成功后获取提交成功提示语"""
-        success = self.element_text(user['获取收货成功提示'])
-        return success
-
-    def text_status(self):
-        """快速收货页面，获取列表第一条记录的最新状态"""
-        Base.presence_sleep_dcr(self, user['获取列表状态'])
-        status = self.element_text(user['获取列表状态'])
-        return status
-
     def click_reset(self):
         """快速收货页面，点击Reset重置按钮"""
         self.is_click(user['Reset'])
