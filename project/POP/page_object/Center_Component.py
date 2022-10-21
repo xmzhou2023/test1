ea43bd7e20c6137f0acf66c501725fc18cea083f
zhang <wenqiang.zhang5@transsion.com>
2022-10-21 15:10:20 +0800

    添加POP用例

--- project/POP/page_object/Center_Component.py
+++ project/POP/page_object/Center_Component.py
@@ -5,13 +5,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import logging
 from project.POP.test_case.conftest import *
+
 object_name = os.path.basename(__file__).split('.')[0]
 user = Element(pro_name, object_name)
 
 class NavPage(Base):
 
     @allure.step("前往菜单")
-    def click_gotonav(self, *content):
+    def click_gotonav(self, *content,):
         """前往菜单"""
         level = []
         navstr = ""
@@ -20,10 +21,10 @@ class NavPage(Base):
             level.append(navstr[2:])
         for i in range(len(content)):
             self.is_click(user[level[i]])
-        sleep(1)
+        sleep(2)
         # 点击重置按钮是为了刷新取消模块浮窗
         self.is_click(user['系统通用重置按钮'])
-        sleep(1)
+        sleep(2)
 
 
 
