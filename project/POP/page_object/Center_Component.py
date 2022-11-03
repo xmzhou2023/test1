35b5b7135765725a3881d22af561093c3885c062
zhang <wenqiang.zhang5@transsion.com>
2022-11-03 09:45:55 +0800

    新增POP用例

--- project/POP/page_object/Center_Component.py
+++ project/POP/page_object/Center_Component.py
@@ -20,11 +20,29 @@ class NavPage(Base):
             navstr = navstr + '->' + content[i]
             level.append(navstr[2:])
         for i in range(len(content)):
-            self.is_click(user[level[i]])
+            self.is_click_tbm(user[level[i]])
         sleep(1)
         # 点击重置按钮是为了刷新取消模块浮窗
         self.is_click(user['系统通用重置按钮'])
         sleep(1)
+
+
+class SpecialNavPage(Base):
+    """特殊模块无重置按钮时点击模块的方法，例：角色管理，用户权限"""
+    @allure.step("前往菜单")
+    def click_gotonav(self, *content,):
+        """前往菜单"""
+        level = []
+        navstr = ""
+        for i in range(len(content)):
+            navstr = navstr + '->' + content[i]
+            level.append(navstr[2:])
+        for i in range(len(content)):
+            self.is_click_tbm(user[level[i]])
+        sleep(1)
+        # 点击刷新是为了刷新取消模块浮窗
+        self.refresh()
+        sleep(5)
 
 
 
