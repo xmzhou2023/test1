90cc2626cd085733802986927989981d03ad369c
Evan <yong.liu7@transsion.com>
2022-11-17 18:05:18 +0800

    update test_10474.py:: Teststory_172:: test_10474

--- project/DRP/test_case/version99__V2_10_8_2计划.py
+++ project/DRP/test_case/version99__V2_10_8_2计划.py
@@ -217,20 +217,19 @@ class Teststory_172:
     @pytest.mark.smoke  # 用例标记
     def test_10474(self, drivers):
         robot = KeyWord(drivers)
-        robot.AI_get("http://10.250.112.166:9000/#/dashboard")
-        robot.AI_find_element(By.CSS_SELECTOR, ".hamburger").click()
+        robot.AI_get("http://10.250.112.166:9000/#/systemManage/userManage")
         robot.AI_find_element(By.XPATH, "//span[contains(.,'系统管理')]").click()
-        robot.AI_find_element(By.XPATH, "//span[contains(.,'用户管理')]").click()
+        robot.AI_find_element(By.XPATH, "//div[13]/ul/div/a/li/span").click()
         robot.AI_find_element(By.XPATH, "(//input[@type='text'])[2]").click()
-        robot.AI_find_element(By.XPATH, "(//input[@type='text'])[2]").send_keys("18647045")
-        robot.AI_find_element(By.XPATH, "//span[contains(.,'18647045')]").click()
+        robot.AI_find_element(By.XPATH, "(//input[@type='text'])[2]").send_keys("18649")
+        robot.AI_find_element(By.XPATH, "//span[contains(.,'18649495')]").click()
         robot.AI_find_element(By.XPATH, "//div[@id='app']/div/div/div[2]/section/div/div/div/form/div/div[3]/button/span").click()
         robot.AI_find_element(By.XPATH, "//div[@id='app']/div/div/div[2]/section/div/div/div/form/div/div[3]/button[2]/span").click()
         robot.AI_find_element(By.XPATH, "(//input[@type='text'])[3]").click()
         robot.AI_find_element(By.XPATH, "(//input[@type='text'])[3]").send_keys("黄")
         robot.AI_find_element(By.XPATH, "//span[contains(.,'黄盼盼')]").click()
         robot.AI_find_element(By.XPATH, "//div[@id='app']/div/div/div[2]/section/div/div/div/form/div/div[3]/button/span").click()
-        robot.AI_find_element(By.XPATH, "(//button[@type='button'])[7]").click()
+        robot.AI_find_element(By.XPATH, "//div[@id='app']/div/div/div[2]/section/div/div/div/form/div/div[3]/button[2]/span").click()
 
 
     @allure.story("用户管理查询界面及功能优化")  # 用户故事名称
