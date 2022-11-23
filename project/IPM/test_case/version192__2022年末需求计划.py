4b3d4a299d1f092626fa1993a87c4a2d2e075571
liumei <mei.liu@transsion.com>
2022-11-23 14:02:42 +0800

    Update version192__2022年末需求计划.py

--- project/IPM/test_case/version192__2022年末需求计划.py
+++ project/IPM/test_case/version192__2022年末需求计划.py
@@ -1,11 +1,24 @@
+import allure
 import pytest
-import time
-import json
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from libs.common.action import KeyWord
+@allure.feature("2022年末需求计划")  # 迭代名称
+class Teststory_2760:
+    @allure.story("高级搜索优化")  # 用户故事名称
+    @allure.title("自动化测试跑流程")  # 用例名称
+    @allure.description("登录==创建项目")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_18778(self, drivers):
+        pass
+
+
+    @allure.story("高级搜索优化")  # 用户故事名称
+    @allure.title("IPM流程测试")  # 用例名称
+    @allure.description("发起==流程审批==断言")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_19625(self, drivers):
+        pass
+
+
+if __name__ == '__main__':
+      pass
