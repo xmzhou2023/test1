1c598f14d6b949ab96eaf035f463bf2d072b343a
Evan <yong.liu7@transsion.com>
2022-11-23 17:53:52 +0800

    update test_18519.py:: Teststory_2467:: test_18519

--- project/DRP/test_case/version144__V3_0_1计划.py
+++ project/DRP/test_case/version144__V3_0_1计划.py
@@ -1,42 +1,11 @@
-import allure
 import pytest
-@allure.feature("V3_0_1计划")  # 迭代名称
-class Teststory_2467:
-    @allure.story("新版DRP权限调整切面部分")  # 用户故事名称
-    @allure.title("测试平台1")  # 用例名称
-    @allure.description("None")  # 用例描述
-    @allure.severity("normal")  # 用例等级
-    @pytest.mark.smoke  # 用例标记
-    def test_18519(self, drivers):
-        pass
-
-
-    @allure.story("新版DRP权限调整切面部分")  # 用户故事名称
-    @allure.title("测试平台2")  # 用例名称
-    @allure.description("到用户管理页面查询工号==到用户管理页面查询姓名")  # 用例描述
-    @allure.severity("normal")  # 用例等级
-    @pytest.mark.smoke  # 用例标记
-    def test_18522(self, drivers):
-        pass
-
-
-    @allure.story("新版DRP权限调整切面部分")  # 用户故事名称
-    @allure.title("测试平台3")  # 用例名称
-    @allure.description("工号查询==姓名查询")  # 用例描述
-    @allure.severity("normal")  # 用例等级
-    @pytest.mark.smoke  # 用例标记
-    def test_18540(self, drivers):
-        pass
-
-
-    @allure.story("新版DRP权限调整切面部分")  # 用户故事名称
-    @allure.title("测试平台4")  # 用例名称
-    @allure.description("查询工号==查询姓名")  # 用例描述
-    @allure.severity("normal")  # 用例等级
-    @pytest.mark.smoke  # 用例标记
-    def test_19966(self, drivers):
-        pass
-
-
-if __name__ == '__main__':
-      pass
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from libs.common.action import KeyWord
