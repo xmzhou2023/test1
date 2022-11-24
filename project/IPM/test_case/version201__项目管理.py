0c28aa61ce2af39cb505b92a9c6f7e48c3eb3205
Evan <yong.liu7@transsion.com>
2022-11-24 11:46:08 +0800

    update IPM: 用例

---
+++ project/IPM/test_case/version201__项目管理.py
@@ -0,0 +1,15 @@
+import allure
+import pytest
+@allure.feature("项目管理")  # 迭代名称
+class Teststory_3259:
+    @allure.story("创建项目")  # 用户故事名称
+    @allure.title("新增项目成功")  # 用例名称
+    @allure.description("点击新增==输入项目信息==保存")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_20708(self, drivers):
+        pass
+
+
+if __name__ == '__main__':
+      pass
