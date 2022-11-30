cf49b328e162dcf6e2abcc99860da96c81c835af
18648901 <hongmei.li@transsion.com>
2022-11-30 19:05:08 +0800

    将Center Process文件里注释的用例解开注释

--- project/DCR/test_case/Center_Process.py
+++ project/DCR/test_case/Center_Process.py
@@ -208,168 +208,155 @@ class TestSalesBusinessProcess:
         ValueAssert.value_assert_equal("Approved", status)
 
 
-# @allure.feature("渠道销售业务流程")
-# class TestDeliveryBusinessProcess:
-#     @allure.story("聚道出库无码产品业务流程")
-#     @allure.title("出库单页面，国包用户，新增无码出库单操作")
-#     @allure.description("出库单页面，国包用户，新增无码出库单操作")
-#     @allure.severity("critical")  # 分别为3种类型等级：critical\normal\minor
-#     @pytest.mark.usefixtures('function_menu_fixture')
-#     def test_002_001(self, drivers):
-#         user2 = LoginPage(drivers)
-#         user2.initialize_login(drivers, "BD40344201", "dcr123456")
-#         """1 国包用户，新增无码出库单操作"""
-#         user2.click_gotomenu("Sales Management", "Delivery Order")
-#
-#         add = DeliveryOrderPage(drivers)
-#         add.click_add()
-#         add.input_sub_buyer("BD2915")
-#         add.input_deli_pay_mode("Online")
-#         add.click_quantity_radio_button()
-#         add.click_quantity_add()
-#         add.click_quantity_product("A2 se BLACK")
-#         add.input_delivery_quantity("1")
-#         add.click_delivery_quantity_text()
-#
-#         """增加断言 检查Delivery Quantity数量是否与输入的数量一致"""
-#         get_deli_quantity = add.get_delivery_quantity_text()
-#         ValueAssert.value_assert_equal("1", get_deli_quantity)
-#
-#         """点击Submit提交按钮"""
-#         add.click_submit()
-#         """获取收货提交成功提示语，断言是否包含Successfully提示语"""
-#         DomAssert(drivers).assert_att("Submit successfully")
-#         sleep(2)
-#         """断言查询新建的无码出库单"""
-#         sql1 = SQL('DCR', 'test')
-#         varsql1 = "select order_code,delivery_code, status from t_channel_delivery_ticket where warehouse_id='62139' and seller_id='1596874516539667' and buyer_id='1596874516539662' and status=80200000 order by created_time desc limit 1"
-#         result = sql1.query_db(varsql1)
-#         order_code = result[0].get("order_code")
-#         delivery_code = result[0].get("delivery_code")
-#         delivery_status = result[0].get("status")
-#         status = add.delivery_convert_status(delivery_status)
-#
-#         """出库单页面，筛选新建的无码出库单ID"""
-#         add.input_salesorder(order_code)
-#         add.input_deliveryorder(delivery_code)
-#         add.click_search()
-#         """出库单列表页面，获取页面，销售单与出库单的文本内容进行筛选"""
-#         get_sales_order = add.text_sales_order()
-#         get_delivery_order = add.text_delivery_order()
-#         get_status = add.text_delivery_Status()
-#         """出库单页面，断言，比较页面获取的文本是否与查询的结果相等"""
-#         ValueAssert.value_assert_equal(get_sales_order, order_code)
-#         ValueAssert.value_assert_equal(get_delivery_order, delivery_code)
-#         ValueAssert.value_assert_equal(get_status, status)
-#
-#
-#         """2 二代用户，快速收货无码出库单"""
-#         user2.initialize_login(drivers, "BD291501", "dcr123456")
-#         """打开Purchase Management菜单"""
-#         user2.click_gotomenu("Purchase Management", "Inbound Receipt")
-#         """二代账号筛选 最近新建的出库单ID，快速收货操作"""
-#         receipt = InboundReceiptPage(drivers)
-#
-#         """从数据库表，查询最近新建的销售单ID与出库单ID"""
-#         sql2 = SQL('DCR', 'test')
-#         varsql2 = "select order_code,delivery_code from t_channel_delivery_ticket  where warehouse_id='62139' and seller_id='1596874516539667' and buyer_id='1596874516539662' and status=80200000 order by created_time desc limit 1"
-#         result = sql2.query_db(varsql2)
-#         order_code = result[0].get("order_code")
-#         delivery_code = result[0].get("delivery_code")
-#
-#         receipt.input_salesOrder(order_code)
-#         receipt.input_deliveryOrder(delivery_code)
-#         """点击筛选查询按钮"""
-#         receipt.click_inbound_receipt_search
-#
-#         receipt.click_checkbox()
-#         receipt.click_quick_received()
-#         receipt.click_save()
-#         """获取收货提交成功提示语，断言是否包含Successfully提示语"""
-#         DomAssert(drivers).assert_att("Successfully")
-#         sleep(2)
-#
-#         """获取列表文本 销售单ID、出库单ID、Status状态"""
-#         salesorder = receipt.get_text_salesOrder()
-#         deliveryorder = receipt.get_text_deliveryOrder()
-#         status = receipt.get_text_status()
-#         """断言二代收货页面 销售单ID、出库单ID是否与数据库查询的数据一致"""
-#         ValueAssert.value_assert_equal(salesorder, order_code)
-#         ValueAssert.value_assert_equal(deliveryorder, delivery_code)
-#         """断言收货后Status：显示GoodsReceipt状态，匹配一致"""
-#         ValueAssert.value_assert_equal("Goods Receipt", status)
-#         receipt.click_close_inbound_receipt()
-#
-#
-#         """刷新页面"""
-#         receipt = SalesOrderPage(drivers)
-#         receipt.click_refresh(drivers)
-#         """二代用户，申请退货无码出库单"""
-#         user2.click_gotomenu("Sales Management", "Return Order")
-#         """实例化 二代退货单类"""
-#         return_order = ReturnOrderPage(drivers)
-#         """退货单列表页面，二代或者零售商用户退货操作"""
-#         """从数据库表，查询国包账号，最近新建的销售单ID与出库单ID"""
-#         sql3 = SQL('DCR', 'test')
-#         varsql3 = "select order_code,delivery_code from t_channel_delivery_ticket  where warehouse_id='62139' and seller_id='1596874516539667' and buyer_id='1596874516539662' and status=80200001 order by created_time desc limit 1"
-#         result = sql3.query_db(varsql3)
-#         delivery_code = result[0].get("delivery_code")
-#         sleep(1)
-#         return_order.click_Add()
-#         return_order.click_Return_Type()
-#         return_order.click_radio_quantity()
-#         return_order.input_quantity_customer("BD2915")
-#         return_order.input_quantity_delivery_order(delivery_code)
-#         return_order.click_quantity_product("A2 se  BLACK")
-#         return_order.input_return_quantity("1")
-#         """点击Check按钮后，断言Order Detail列表记录是否正确"""
-#         return_order.click_Check()
-#         get_quantity_deli = return_order.get_quantity_deli_order_text(delivery_code)
-#         ValueAssert.value_assert_equal(get_quantity_deli, delivery_code)
-#
-#         get_seller_id = return_order.get_quantity_seller_id_text("BD403442")
-#         ValueAssert.value_assert_equal("BD403442", get_seller_id)
-#         get_buyer_id = return_order.get_quantity_buyer_id_text("BD2915")
-#         ValueAssert.value_assert_equal("BD2915", get_buyer_id)
-#         """点击提交"""
-#         return_order.click_Submit()
-#         DomAssert(drivers).assert_att("Submit Success!")
-#
-#         """方法参数赋值给变量"""
-#         return_order.input_Delivery_Orderid(delivery_code)
-#         return_order.click_Search()
-#
-#         """筛选退货列表页，获取退货出库单ID文本 与数据库表中查询的出库单ID对比是否一致"""
-#         delivery_order_id = return_order.get_text_deliveryID()
-#         status = return_order.get_return_status()
-#         ValueAssert.value_assert_equal(delivery_order_id, delivery_code)
-#         ValueAssert.value_assert_equal("Pending Approval", status)
-#
-#
-#         """4 退货单页面，国包用户，审核无码退货单操作"""
-#         user2.initialize_login(drivers, "BD40344201", "dcr123456")
-#         """打开Purchase Management菜单"""
-#         user2.click_gotomenu("Sales Management", "Return Order")
-#         """实例化 Return order退货单类"""
-#         return_approve = ReturnOrderPage(drivers)
-#         """从数据库表，查询国包账号，最近新建的销售单ID与出库单ID"""
-#         sql4 = SQL('DCR', 'test')
-#         varsql4 = "select order_code,delivery_code from t_channel_delivery_ticket  where warehouse_id='62139' and seller_id='1596874516539667' and buyer_id='1596874516539662' and status=80200001 order by created_time desc limit 1"
-#         result = sql4.query_db(varsql4)
-#         delivery_code = result[0].get("delivery_code")
-#
-#         return_approve.input_Delivery_Orderid(delivery_code)
-#         return_approve.click_Search()
-#
-#         return_approve.click_checkbox()
-#         return_approve.click_Approve_button()
-#         return_approve.input_remark("同意退货")
-#         return_approve.click_agree()
-#         """ 断言页面是否存在审核成功Approval successfully文本 """
-#         DomAssert(drivers).assert_att("Approval successfully")
-#         """退货成功后，获取列表第一个状态，断言判断是否审核成功"""
-#         status = return_approve.get_text_Status()
-#         ValueAssert.value_assert_equal("Approved", status)
+@allure.feature("渠道销售业务流程")
+class TestDeliveryBusinessProcess:
+    @allure.story("聚道出库无码产品业务流程")
+    @allure.title("出库单页面，国包用户，新增无码出库单操作")
+    @allure.description("出库单页面，国包用户，新增无码出库单操作")
+    @allure.severity("critical")  # 分别为3种类型等级：critical\normal\minor
+    @pytest.mark.usefixtures('function_menu_fixture')
+    def test_002_001(self, drivers):
+        user2 = LoginPage(drivers)
+        user2.initialize_login(drivers, "BD40344201", "dcr123456")
+        """1 国包用户，新增无码出库单操作"""
+        user2.click_gotomenu("Sales Management", "Delivery Order")
+        add = DeliveryOrderPage(drivers)
+        add.click_add()
+        add.input_sub_buyer("BD2915")
+        add.input_deli_pay_mode("Online")
+        add.click_quantity_radio_button()
+        add.click_quantity_add()
+        add.click_quantity_product("A2 se BLACK")
+        add.input_delivery_quantity("1")
+        add.click_delivery_quantity_text()
+        """增加断言 检查Delivery Quantity数量是否与输入的数量一致"""
+        get_deli_quantity = add.get_delivery_quantity_text()
+        ValueAssert.value_assert_equal("1", get_deli_quantity)
+        """点击Submit提交按钮"""
+        add.click_submit()
+        """获取收货提交成功提示语，断言是否包含Successfully提示语"""
+        DomAssert(drivers).assert_att("Submit successfully")
+        sleep(2)
+        """断言查询新建的无码出库单"""
+        sql1 = SQL('DCR', 'test')
+        varsql1 = "select order_code,delivery_code, status from t_channel_delivery_ticket where warehouse_id='62139' and seller_id='1596874516539667' and buyer_id='1596874516539662' and status=80200000 order by created_time desc limit 1"
+        result = sql1.query_db(varsql1)
+        order_code = result[0].get("order_code")
+        delivery_code = result[0].get("delivery_code")
+        delivery_status = result[0].get("status")
+        status = add.delivery_convert_status(delivery_status)
+        """出库单页面，筛选新建的无码出库单ID"""
+        add.input_salesorder(order_code)
+        add.input_deliveryorder(delivery_code)
+        add.click_search()
+        """出库单列表页面，获取页面，销售单与出库单的文本内容进行筛选"""
+        get_sales_order = add.text_sales_order()
+        get_delivery_order = add.text_delivery_order()
+        get_status = add.text_delivery_Status()
+        """出库单页面，断言，比较页面获取的文本是否与查询的结果相等"""
+        ValueAssert.value_assert_equal(get_sales_order, order_code)
+        ValueAssert.value_assert_equal(get_delivery_order, delivery_code)
+        ValueAssert.value_assert_equal(get_status, status)
+
+
+        """2 二代用户，快速收货无码出库单"""
+        user2.initialize_login(drivers, "BD291501", "dcr123456")
+        """打开Purchase Management菜单"""
+        user2.click_gotomenu("Purchase Management", "Inbound Receipt")
+        """二代账号筛选 最近新建的出库单ID，快速收货操作"""
+        receipt = InboundReceiptPage(drivers)
+        """从数据库表，查询最近新建的销售单ID与出库单ID"""
+        sql2 = SQL('DCR', 'test')
+        varsql2 = "select order_code,delivery_code from t_channel_delivery_ticket  where warehouse_id='62139' and seller_id='1596874516539667' and buyer_id='1596874516539662' and status=80200000 order by created_time desc limit 1"
+        result = sql2.query_db(varsql2)
+        order_code = result[0].get("order_code")
+        delivery_code = result[0].get("delivery_code")
+        receipt.input_salesOrder(order_code)
+        receipt.input_deliveryOrder(delivery_code)
+        """点击筛选查询按钮"""
+        receipt.click_inbound_receipt_search
+        receipt.click_checkbox()
+        receipt.click_quick_received()
+        receipt.click_save()
+        """获取收货提交成功提示语，断言是否包含Successfully提示语"""
+        DomAssert(drivers).assert_att("Successfully")
+        sleep(2)
+        """获取列表文本 销售单ID、出库单ID、Status状态"""
+        salesorder = receipt.get_text_salesOrder()
+        deliveryorder = receipt.get_text_deliveryOrder()
+        status = receipt.get_text_status()
+        """断言二代收货页面 销售单ID、出库单ID是否与数据库查询的数据一致"""
+        ValueAssert.value_assert_equal(salesorder, order_code)
+        ValueAssert.value_assert_equal(deliveryorder, delivery_code)
+        """断言收货后Status：显示GoodsReceipt状态，匹配一致"""
+        ValueAssert.value_assert_equal("Goods Receipt", status)
+        receipt.click_close_inbound_receipt()
+
+
+        """刷新页面"""
+        receipt = SalesOrderPage(drivers)
+        receipt.click_refresh(drivers)
+        """二代用户，申请退货无码出库单"""
+        user2.click_gotomenu("Sales Management", "Return Order")
+        """实例化 二代退货单类"""
+        return_order = ReturnOrderPage(drivers)
+        """退货单列表页面，二代或者零售商用户退货操作"""
+        """从数据库表，查询国包账号，最近新建的销售单ID与出库单ID"""
+        sql3 = SQL('DCR', 'test')
+        varsql3 = "select order_code,delivery_code from t_channel_delivery_ticket  where warehouse_id='62139' and seller_id='1596874516539667' and buyer_id='1596874516539662' and status=80200001 order by created_time desc limit 1"
+        result = sql3.query_db(varsql3)
+        delivery_code = result[0].get("delivery_code")
+        sleep(1)
+        return_order.click_Add()
+        return_order.click_Return_Type()
+        return_order.click_radio_quantity()
+        return_order.input_quantity_customer("BD2915")
+        return_order.input_quantity_delivery_order(delivery_code)
+        return_order.click_quantity_product("A2 se  BLACK")
+        return_order.input_return_quantity("1")
+        """点击Check按钮后，断言Order Detail列表记录是否正确"""
+        return_order.click_Check()
+        get_quantity_deli = return_order.get_quantity_deli_order_text(delivery_code)
+        ValueAssert.value_assert_equal(get_quantity_deli, delivery_code)
+        get_seller_id = return_order.get_quantity_seller_id_text("BD403442")
+        ValueAssert.value_assert_equal("BD403442", get_seller_id)
+        get_buyer_id = return_order.get_quantity_buyer_id_text("BD2915")
+        ValueAssert.value_assert_equal("BD2915", get_buyer_id)
+        """点击提交"""
+        return_order.click_Submit()
+        DomAssert(drivers).assert_att("Submit Success!")
+        """方法参数赋值给变量"""
+        return_order.input_Delivery_Orderid(delivery_code)
+        return_order.click_Search()
+        """筛选退货列表页，获取退货出库单ID文本 与数据库表中查询的出库单ID对比是否一致"""
+        delivery_order_id = return_order.get_text_deliveryID()
+        status = return_order.get_return_status()
+        ValueAssert.value_assert_equal(delivery_order_id, delivery_code)
+        ValueAssert.value_assert_equal("Pending Approval", status)
+
+
+        """4 退货单页面，国包用户，审核无码退货单操作"""
+        user2.initialize_login(drivers, "BD40344201", "dcr123456")
+        """打开Purchase Management菜单"""
+        user2.click_gotomenu("Sales Management", "Return Order")
+        """实例化 Return order退货单类"""
+        return_approve = ReturnOrderPage(drivers)
+        """从数据库表，查询国包账号，最近新建的销售单ID与出库单ID"""
+        sql4 = SQL('DCR', 'test')
+        varsql4 = "select order_code,delivery_code from t_channel_delivery_ticket  where warehouse_id='62139' and seller_id='1596874516539667' and buyer_id='1596874516539662' and status=80200001 order by created_time desc limit 1"
+        result = sql4.query_db(varsql4)
+        delivery_code = result[0].get("delivery_code")
+        return_approve.input_Delivery_Orderid(delivery_code)
+        return_approve.click_Search()
+        return_approve.click_checkbox()
+        return_approve.click_Approve_button()
+        return_approve.input_remark("同意退货")
+        return_approve.click_agree()
+        """ 断言页面是否存在审核成功Approval successfully文本 """
+        DomAssert(drivers).assert_att("Approval successfully")
+        """退货成功后，获取列表第一个状态，断言判断是否审核成功"""
+        status = return_approve.get_text_Status()
+        ValueAssert.value_assert_equal("Approved", status)
 
 
 if __name__ == '__main__':
