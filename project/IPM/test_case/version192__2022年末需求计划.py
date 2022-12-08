643cd4bcd91f5bf0251d1ff98261b92551250879
Evan <yong.liu7@transsion.com>
2022-12-08 09:41:54 +0800

    update IPM: 用例

--- project/IPM/test_case/version192__2022年末需求计划.py
+++ project/IPM/test_case/version192__2022年末需求计划.py
@@ -2,7 +2,7 @@ import allure
 import pytest
 @allure.feature("2022年末需求计划")  # 迭代名称
 class Teststory_2760:
-    @allure.story(""""高级搜索优化"""")  # 用户故事名称
+    @allure.story("""""高级搜索优化""""")  # 用户故事名称
     @allure.title("自动化测试跑流程")  # 用例名称
     @allure.description("登录==创建项目")  # 用例描述
     @allure.severity("normal")  # 用例等级
@@ -11,7 +11,7 @@ class Teststory_2760:
         pass
 
 
-    @allure.story(""""高级搜索优化"""")  # 用户故事名称
+    @allure.story("""""高级搜索优化""""")  # 用户故事名称
     @allure.title("IPM流程测试")  # 用例名称
     @allure.description("发起==流程审批==断言")  # 用例描述
     @allure.severity("normal")  # 用例等级
