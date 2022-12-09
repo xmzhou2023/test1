2f9738886e1ab9151f0f6b95a5d437c0a5ce7b1a
liumei <mei.liu@transsion.com>
2022-12-09 16:02:54 +0800

    Update version110__V3_0_0计划.py

--- project/DRP/test_case/version110__V3_0_0计划.py
+++ project/DRP/test_case/version110__V3_0_0计划.py
@@ -41,24 +41,7 @@ class Teststory_1811:
     @allure.severity("critical")  # 用例等级
     @pytest.mark.smoke  # 用例标记
     def test_10575(self, drivers):
-        robot = KeyWord(drivers)
-        robot.AI_get("http://10.250.112.57/")#id77287147-4b13-4d95-bbeb-45bb76c4b771
-        robot.AI_find_element(By.CSS_SELECTOR, ".hamburger").click()#id28c141ed-a7ac-47eb-96e0-b85742419d86
-        robot.AI_find_element(By.CSS_SELECTOR, "div:nth-child(4) > .el-submenu span:nth-child(2)").click()#id617ad7d4-fdbc-4e0b-ad6c-85a29d86df39
-        robot.AI_find_element(By.CSS_SELECTOR, ".is-opened .nest-menu:nth-child(1) .el-submenu__title > span").click()#id7a54076f-56c8-4ba6-b9e5-5788dc64482d
-        robot.AI_find_element(By.CSS_SELECTOR, ".is-opened > .el-menu > .nest-menu:nth-child(1) > .el-menu-item > span").click()#ide2a89d0d-db17-48d2-9bfb-036a2fd523a7
-        robot.AI_find_element(By.CSS_SELECTOR, "div:nth-child(2) > .el-button:nth-child(1) > span").click()#id983e7462-7b79-4803-933c-ed1e8dabf262
-        element = robot.AI_find_element(By.CSS_SELECTOR, "div:nth-child(2) > .el-button:nth-child(1) > span")#id228184c7-7de4-414f-bbdd-cfe2c9b75723
-        actions = ActionChains(robot)
-        actions.move_to_element(element).perform()
-        element = robot.AI_find_element(By.CSS_SELECTOR, "body")#id5d32d7de-27e1-4dd4-9cb5-da510be407d7
-        actions = ActionChains(robot)
-        actions.move_to_element(element, 0, 0).perform()
-        element = robot.AI_find_element(By.CSS_SELECTOR, ".el-button--warning")#id8d540683-e189-47e6-90c0-f2eebe681ee4
-        actions = ActionChains(robot)
-        actions.move_to_element(element).perform()
-        robot.AI_find_element(By.CSS_SELECTOR, ".hover > span").click()#id7d94c671-4405-4e90-be48-05210aeb4ba3
-        robot.AI_find_element(By.CSS_SELECTOR, ".hover > span").click()#id9d7be00c-0bfc-469a-b0b4-a03d2dc0eda9
+        pass
 
 
 if __name__ == '__main__':
