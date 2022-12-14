e0a12e1709787f26274a50e4a89d8049e6133d84
Evan <yong.liu7@transsion.com>
2022-12-14 10:58:52 +0800

    update DRP: 用例

--- project/DRP/test_case/version110__V3_0_0计划.py
+++ project/DRP/test_case/version110__V3_0_0计划.py
@@ -21,6 +21,15 @@ class Teststory_1786:
         pass
 
 
+    @allure.story("DRP2国家销售版增加【日志】功能")  # 用户故事名称
+    @allure.title("DRP管理DRP2国家销售版增加【日志】功能编辑")  # 用例名称
+    @allure.description("通过菜单路径DRP管理系统gtDRP管理gtDRP2国家销售版进入页面。==选择一条数据，点击【编辑】按钮")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_10566(self, drivers):
+        pass
+
+
 @allure.feature("V3_0_0计划")  # 迭代名称
 class Teststory_1811:
     @allure.story("DRP提报批量提交功能支持校验oneworks审批流程是否已经发起")  # 用户故事名称
