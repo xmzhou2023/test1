218f9e0e1fcd50f9f5f7791ffc7626146a581f8d
Evan <yong.liu7@transsion.com>
2022-12-23 17:48:30 +0800

    update DRP: 用例

--- project/DRP/test_case/version144__V3_0_1计划.py
+++ project/DRP/test_case/version144__V3_0_1计划.py
@@ -13,8 +13,8 @@ import allure
 @allure.feature("V3_0_1计划")  # 迭代名称
 class Teststory_2467:
     @allure.story("新版DRP权限调整切面部分")  # 用户故事名称
-    @allure.title("测试平台1")  # 用例名称
-    @allure.description("")  # 用例描述
+    @allure.title("测试平台4")  # 用例名称
+    @allure.description("查询工号==查询姓名")  # 用例描述
     @allure.severity("normal")  # 用例等级
     @pytest.mark.smoke  # 用例标记
     def test_18519(self, drivers):
@@ -37,7 +37,32 @@ def test_19966(self, drivers):
         robot.AI_get("http://10.250.112.166:9000//")#id2fec00db-f7f9-4dd4-9795-0a93768c6d55
         robot.close()
 
+    @allure.story("新版DRP权限调整切面部分")  # 用户故事名称
+    @allure.title("测试平台1")  # 用例名称
+    @allure.description("")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_18519(self, drivers):
+        pass
+
+
+    @allure.story("新版DRP权限调整切面部分")  # 用户故事名称
+    @allure.title("测试平台2")  # 用例名称
+    @allure.description("到用户管理页面查询工号==到用户管理页面查询姓名")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_18522(self, drivers):
+        pass
+
+
+    @allure.story("新版DRP权限调整切面部分")  # 用户故事名称
+    @allure.title("测试平台3")  # 用例名称
+    @allure.description("工号查询==姓名查询")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_18540(self, drivers):
+        pass
+
+
 if __name__ == '__main__':
     pass
-
-
