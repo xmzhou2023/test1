0def971818a9d6659db576352b36537e0303fdc6
wenminxiong <xwm101827>
2022-12-28 10:18:09 +0800

    删除空文件

--- project/CRM/test_case/RepairMgt_WOSerializedMgt_ReturnSpareFromTechnician.py
+++ project/CRM/test_case/RepairMgt_WOSerializedMgt_ReturnSpareFromTechnician.py
@@ -10,15 +10,3 @@ import pytest
         trivial级别:轻微缺陷(必输项无提示， 或者提示不规范)
 """
 
-@allure.feature("脚本名称") # 模块名称
-class TestUtil:
-    @allure.story("二级标题") # 场景名称
-    @allure.title("三级标题")  # 用例名称
-    @allure.description("用例描述")
-    @allure.severity("normal")  # 用例等级
-    @pytest.mark.smoke # 用例标记
-    def test_001_001(self, drivers):   # 用例名称取名规范'test+场景编号+用例编号'
-        pass
-
-if __name__ == '__main__':
-    pytest.main(['project/DRP/testcase/run_code.py'])
