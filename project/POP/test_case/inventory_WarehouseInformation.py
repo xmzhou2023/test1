3a36e74fdab4558607c51654b78f969c5d257ae9
Liu Su <pop11237>
2022-12-30 17:47:18 +0800

    新增自采商品收货

--- project/POP/test_case/inventory_WarehouseInformation.py
+++ project/POP/test_case/inventory_WarehouseInformation.py
@@ -3,7 +3,7 @@ import time
 import allure
 import pytest
 from project.POP.page_object.Center_Component import SpecialNavPage
-from project.POP.page_object.inventory_WarehouseInformation import WarehouseInformation
+from project.POP.page_object.inventory_WarehouseInformation import *
 from project.POP.test_case.conftest import *
 from libs.common.read_element import Element
 
@@ -28,16 +28,36 @@ class TestAddWarehouse:
     @pytest.mark.smoke  # 用例标记
     def test_001_001(self, drivers):  # 用例名称取名规范'test+场景编号+用例编号'
         users = WarehouseInformation(drivers)
-        users.warehouse_add('新增')
+        users.warehouse_menu('新增')
         num = str(int(time.time()))
-        users.warehouse_warehousename('中国' + num)
+        users.warehouse_warehouse_name('中国' + num)
         sleep()
-        users.warehouse_shopname('南京')
+        users.warehouse_shop_name('南京')
         users.warehouse_select()
         users.click_submit('提交')
         sleep(3)
         test = users.element_text(user['新增成功提示'])
         ValueAssert.value_assert_equal(test, '新增成功')
 
+
+class TestEditWarehouse:
+    @allure.story("编辑仓库名")
+    @allure.title("编辑分仓名称")
+    @allure.description("修改仓库名为已存在的仓库名")
+    @allure.severity("normal")
+    @pytest.mark.smoke
+    def test_002_001(self, drivers):
+        users = SelectWarehouseInformation(drivers)
+        users.query_warehouse_information('仓库框', '中国')
+        sleep(3)
+        edit = WarehouseInformation(drivers)
+        edit.click_search_or_reset('查询')
+        sleep(3)
+        edit.click_checkbox()
+        edit.warehouse_menu('编辑')
+        edit.warehouse_warehouse_name('中国2')
+        edit.click_submit('提交')
+
+
 if __name__ == '__main__':
-    pytest.main(['inventory_WarehouseInformation.py'])
+    pytest.main(['inventory_WarehouseInformation.py::TestEditWarehouse'])
