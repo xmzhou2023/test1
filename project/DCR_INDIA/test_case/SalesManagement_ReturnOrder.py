6d64b5d96821649c825696a1d99a93cadd68e1e6
18648901 <hongmei.li@transsion.com>
2023-01-03 17:36:46 +0800

    每个用例中补充用例标记

--- project/DCR_INDIA/test_case/SalesManagement_ReturnOrder.py
+++ project/DCR_INDIA/test_case/SalesManagement_ReturnOrder.py
@@ -23,6 +23,7 @@ class TestReturnOrder:
     @allure.title("随机条件组合查询退货单")
     @allure.description("退货单页面，查询退货单的随机条件组合查询")
     @allure.severity("critical")  # 分别为3种类型等级：critical\normal\minor
+    @pytest.mark.smoke  # 用例标记
     @pytest.mark.usefixtures('function_menu_fixture')
     def test_001_001(self, drivers):
         Base(drivers).refresh()
