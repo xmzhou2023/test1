393137e72b948329d883368a7fbbf80ab41b9505
xingyu.zhu <Zxy18100875832>
2023-01-04 17:02:11 +0800

    POP

--- project/POP/test_case/Commodity_price_record.py
+++ project/POP/test_case/Commodity_price_record.py
@@ -18,10 +18,27 @@ class TestQueryCommodityPrice:
     def test_001_001(self, drivers):   # 用例名称取名规范'test+场景编号+用例编号'
         users = QueryCommodityPrice(drivers)
         users.querycommodityprice("商品价格记录-商品名称框","PA00005434")
-        sleep(2)
+        sleep(5)
         # 断言--查询商品名称与输入的商品名称一致
         test = users.element_text(user['商品名称断言'])
         ValueAssert.value_assert_equal(test,"itel A22 8+1 CHAMPAGNE GOLD")
+
+
+    @allure.story("商品价格记录") # 场景名称
+    @allure.title("查询商品价格记录")  # 用例名称
+    @allure.description("输入产品名称，点击查询对应定义价格记录数据")
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke # 用例标记
+    def test_001_002(self, drivers):   # 用例名称取名规范'test+场景编号+用例编号'
+        users = QueryCommodityPrice(drivers)
+        users.querycommodityprice("商品价格记录-产品名称框","POP 3")
+        sleep(3)
+        # 断言--查询商品名称与输入的商品名称一致
+        test = users.element_text(user['产品名称断言'])
+        ValueAssert.value_assert_equal(test,"POP 3")
+
+
+
 
 if __name__ == '__main__':
     pytest.main(['Commodity_price_record.py::TestQueryCommodityPrice'])
