24fe11a4b9872a317e21e6f4d6d53b5eedcb86f2
xingyu.zhu <Zxy18100875832>
2023-01-05 17:36:35 +0800

    POP

--- libs/tools/create_dir.py
+++ libs/tools/create_dir.py
@@ -9,7 +9,7 @@ import os
 import shutil
 
 # 自动新增文件夹
-PERO_PATH = os.path.join(PEROJECT_PATH, 'TranDesk')
+PERO_PATH = os.path.join(PEROJECT_PATH, 'POP')
 # 创建文件依据
 path = os.path.join('template/modle_name.csv')
 
@@ -63,9 +63,9 @@ def text_create(name, type="case"):
     else:
         desktop_path = CASE_PATH     # 新创建的txt文件的存放路径
         mk_dir(desktop_path)
-        # full_path = desktop_path + name + '.py'
-        # msg = read_file(CASE_TEMPLATE_PATH)
-        # mk_file(full_path, msg)
+        full_path = desktop_path + name + '.py'
+        msg = read_file(CASE_TEMPLATE_PATH)
+        mk_file(full_path, msg)
         conftest_path = desktop_path + 'conftest.py'
         msg = read_file(CONFTEST_TEMPLATE_PATH)
         mk_file(conftest_path, msg)
@@ -84,8 +84,8 @@ def generate_env():
 if __name__ == '__main__':
     generate_module("element")
     generate_module("object")
-    generate_module("testcase")
-    generate_env()
+    generate_module("test_case")
+    # generate_env()
 
 
 
