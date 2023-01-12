8966d9a85046e4cc79ad154e08e8bfed495b0a85
Evan <yong.liu7@transsion.com>
2023-01-12 14:03:19 +0800

    update TranDesk: 用例

---
+++ project/TranDesk/test_case/version262__迭代名称异常字符串测试039____测试测试哦/自动化管理_37_262.py
@@ -0,0 +1,15 @@
+import allure
+import pytest
+@allure.feature("自动化管理_37")  # 迭代名称
+class Teststory_4454:
+    @allure.story("测试测试__039自动化平台测试_防静电设计费开始卷发卡机快放假撒可富几十块打飞机卡萨分接口撒九分裤撒酒疯分接口时间开发__")  # 用户故事名称
+    @allure.title("测试一异常字符串运行")  # 用例名称
+    @allure.description("步骤1:就分开加开发;步骤2:'快放假电视fdsfsdfsfsd槛风数据库';步骤3:我人哦诶我;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_31137(self, drivers):
+        pass
+
+
+if __name__ == '__main__':
+      pass
