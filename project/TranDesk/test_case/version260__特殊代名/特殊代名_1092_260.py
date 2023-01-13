1870afff86a84ed6b987128ce8bbbb62fc9ffcc8
Evan <yong.liu7@transsion.com>
2023-01-13 15:29:15 +0800

    update TranDesk: 用例

--- project/TranDesk/test_case/version260__特殊代名/特殊代名_1092_260.py
+++ project/TranDesk/test_case/version260__特殊代名/特殊代名_1092_260.py
@@ -31,5 +31,16 @@ class Teststory_4443:
         pass
 
 
+@allure.feature("特殊代名_1092")  # 迭代名称
+class Teststory_4431:
+    @allure.story("需_求_名_称_异.常处理测试")  # 用户故事名称
+    @allure.title("测试1")  # 用例名称
+    @allure.description("步骤1:测试1;步骤2:测试2;步骤3:测试3;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_30733(self, drivers):
+        pass
+
+
 if __name__ == '__main__':
-      pass
+    pass
