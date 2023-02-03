98c30233b1c7b732c67ef66a7c32b9364c25e90a
Evan <yong.liu7@transsion.com>
2023-02-03 16:30:18 +0800

    update TranDesk: 用例

---
+++ project/TranDesk/test_case/version246__B001R001V002SP008/自动化管理新增模块_87_246.py
@@ -0,0 +1,31 @@
+import allure
+import pytest
+@allure.feature("自动化管理新增模块_37")  # 迭代名称
+class Teststory_4208:
+    @allure.story("平台整体功能联调与维稳")  # 用户故事名称
+    @allure.title("测测测1")  # 用例名称
+    @allure.description("步骤1:步骤1;步骤2:步骤2;步骤3:步骤3;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_14(self, drivers):
+        print('123')
+
+    @allure.story("平台整体功能联调与维稳")  # 用户故事名称
+    @allure.title("测测测2")  # 用例名称
+    @allure.description("步骤1:步骤1;步骤2:步骤2;步骤3:步骤3;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_15(self, drivers):
+        print('123')
+
+    @allure.story("平台整体功能联调与维稳")  # 用户故事名称
+    @allure.title("测测测3")  # 用例名称
+    @allure.description("步骤1:步骤1;步骤2:步骤2;步骤3:步骤3;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_16(self, drivers):
+        print('123')
+
+
+if __name__ == '__main__':
+      pass
