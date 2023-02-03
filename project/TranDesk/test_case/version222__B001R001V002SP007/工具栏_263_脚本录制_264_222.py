66a0e3c87b1c215fb4f61251f17fe5b21e3aebb0
Evan <yong.liu7@transsion.com>
2023-02-03 17:42:50 +0800

    修改场景名，用例名一样

--- project/TranDesk/test_case/version222__B001R001V002SP007/工具栏_263_脚本录制_264_222.py
+++ project/TranDesk/test_case/version222__B001R001V002SP007/工具栏_263_脚本录制_264_222.py
@@ -10,15 +10,15 @@ class Teststory_3791:
     def test_24861(self, drivers):
         print('1111')
 
-    @allure.story("查找到指定用户并配置菜单权限")  # 用户故事名称
-    @allure.title("333333333")  # 用例名称
+    @allure.story("测试删除需求2")  # 用户故事名称
+    @allure.title("查找到指定用户并配置菜单权限")  # 用例名称
     @allure.description("")  # 用例描述
     @allure.severity("normal")  # 用例等级
     @pytest.mark.smoke  # 用例标记
     def test_25940(self, drivers):
         print('2222')
 
-    @allure.story("查找到指定用户并配置菜单权限")  # 用户故事名称
+    @allure.story("测试删除需求2")  # 用户故事名称
     @allure.title("4444444")  # 用例名称
     @allure.description("")  # 用例描述
     @allure.severity("normal")  # 用例等级
