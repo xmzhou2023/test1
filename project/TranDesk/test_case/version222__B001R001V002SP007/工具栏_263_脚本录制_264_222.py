d0bc9b7010d5224fbb32762319c3b42ab4da19b7
Evan <yong.liu7@transsion.com>
2023-02-03 17:11:10 +0800

    修改25930上边，25928下边

--- project/TranDesk/test_case/version222__B001R001V002SP007/工具栏_263_脚本录制_264_222.py
+++ project/TranDesk/test_case/version222__B001R001V002SP007/工具栏_263_脚本录制_264_222.py
@@ -11,14 +11,6 @@ class Teststory_3791:
         print('1111')
 
 
-    @allure.story("测试删除需求2")  # 用户故事名称
-    @allure.title("111111111111111111111")  # 用例名称
-    @allure.description("")  # 用例描述
-    @allure.severity("normal")  # 用例等级
-    @pytest.mark.smoke  # 用例标记
-    def test_25928(self, drivers):
-        print('2222')
-
     @allure.story("测试删除需求3")  # 用户故事名称
     @allure.title("22222222222222")  # 用例名称
     @allure.description("")  # 用例描述
@@ -27,6 +19,14 @@ class Teststory_3791:
     def test_25930(self, drivers):
         print('2222')
 
+
+    @allure.story("测试删除需求2")  # 用户故事名称
+    @allure.title("111111111111111111111")  # 用例名称
+    @allure.description("")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_25928(self, drivers):
+        print('2222')
 
 if __name__ == '__main__':
       pass
