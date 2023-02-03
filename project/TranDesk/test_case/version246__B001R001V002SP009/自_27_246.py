c398948996be5b6c04514130113b56471efa5bf1
Evan <yong.liu7@transsion.com>
2023-02-03 15:59:02 +0800

    添加TranTest测试用例

---
+++ project/TranDesk/test_case/version246__B001R001V002SP009/自_27_246.py
@@ -0,0 +1,31 @@
+import allure
+import pytest
+@allure.feature("自_27_246")  # 迭代名称
+class Teststory_4207:
+    @allure.story("平台整体功能联调与维稳")  # 用户故事名称
+    @allure.title("测测测1")  # 用例名称
+    @allure.description("步骤1:步骤1;步骤2:步骤2;步骤3:步骤3;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_1(self, drivers):
+        print('123')
+
+    @allure.story("平台整体功能联调与维稳")  # 用户故事名称
+    @allure.title("测测测2")  # 用例名称
+    @allure.description("步骤1:步骤1;步骤2:步骤2;步骤3:步骤3;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_2(self, drivers):
+        print('123')
+
+    @allure.story("平台整体功能联调与维稳")  # 用户故事名称
+    @allure.title("测测测3")  # 用例名称
+    @allure.description("步骤1:步骤1;步骤2:步骤2;步骤3:步骤3;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_3(self, drivers):
+        print('123')
+
+
+if __name__ == '__main__':
+      pass
---
+++ project/TranDesk/test_case/version246__B001R001V002SP009/自动化管理_37_场景管理_259_246.py
@@ -0,0 +1,103 @@
+import allure
+import pytest
+@allure.feature("自动化管理修改模块描述_37_任务管理_260")  # 迭代名称
+class Teststory_4078:
+
+    @allure.story("用例管理迭代需求CRM试验")  # 用户故事名称
+    @allure.title("修改用例名称111111111111111111")  # 用例名称
+    @allure.description("修改用例描述")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_7(self, drivers):
+        print('124')  #修改用例内容
+
+    @allure.story("用例管理迭代需求CRM试验")  # 用户故事名称
+    @allure.title("新增用例名称333333333333333")  # 用例名称
+    @allure.description("修改用例描述")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_777(self, drivers):
+        print('124')  # 修改用例内容
+
+    @allure.story("用例管理迭代需求CRM试验")  # 用户故事名称
+    @allure.title("新增用例11111111")  # 用例名称
+    @allure.description("步骤1:程序执行步骤1;步骤2:程序执行步骤二;步骤3:程序执行步骤三;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_11111(self, drivers):
+        print('124')  # 修改用例内容
+
+    @allure.story("用例管理迭代需求CRM试验")  # 用户故事名称
+    @allure.title("传测自动化测试用例5")  # 用例名称
+    @allure.description("修改用例描述")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_8(self, drivers):
+        print('124')   #修改用例内容
+
+    @allure.story("用例管理迭代需求CRM试验")  # 用户故事名称
+    @allure.title("新增5")  # 用例名称
+    @allure.description("步骤1:程序执行步骤1;步骤2:程序执行步骤二;步骤3:程序执行步骤三;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_110(self, drivers):
+        print('123')
+
+
+
+
+@allure.feature("自动化管理修改模块描述_37_任务管理_260")  # 迭代名称
+class Teststory_4079:
+    @allure.story("用例管理迭代需求CRM试验2")  # 用户故事名称
+    @allure.title("TranTest自动化测试用例1")  # 用例名称
+    @allure.description("步骤1:步骤一内容;步骤2:步骤二内容;步骤3:步骤三内容;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_9(self, drivers):
+        print('123')
+
+    @allure.story("用例管理迭代需求CRM试验2")  # 用户故事名称
+    @allure.title("TranTest自动化测试用例4")  # 用例名称
+    @allure.description("步骤1:步骤一内容;步骤2:步骤二内容;步骤3:步骤三内容;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_99(self, drivers):
+        print('123')
+
+
+
+    @allure.story("用例管理迭代需求CRM试验2")  # 用户故事名称
+    @allure.title("传测自动化测试用例5")  # 用例名称
+    @allure.description("步骤1:程序执行步骤1;步骤2:程序执行步骤二;步骤3:程序执行步骤三;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_101(self, drivers):
+        print('123')
+
+
+    @allure.story("用例管理迭代需求CRM试验2")  # 用户故事名称
+    @allure.title("修改测试用例5")  # 用例名称
+    @allure.description("步骤1:程序执行步骤1;步骤2:程序执行步骤二;步骤3:程序执行步骤三;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_13(self, drivers):
+        print('123')
+
+    @allure.story("用例管理迭代需求CRM试验2")  # 用户故事名称
+    @allure.title("修改测试用例名称")  # 用例名称
+    @allure.description("步骤1:程序执行步骤1;步骤2:程序执行步骤二;步骤3:程序执行步骤三;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_1111111113(self, drivers):
+        print('123')
+
+    @allure.story("用例管理迭代需求CRM试验2")  # 用户故事名称
+    @allure.title("新增测试用例5555555555")  # 用例名称
+    @allure.description("步骤1:程序执行步骤1;步骤2:程序执行步骤二;步骤3:程序执行步骤三;")  # 用例描述
+    @allure.severity("normal")  # 用例等级
+    @pytest.mark.smoke  # 用例标记
+    def test_1111111114(self, drivers):
+        print('123')
+
+if __name__ == '__main__':
+      pass
