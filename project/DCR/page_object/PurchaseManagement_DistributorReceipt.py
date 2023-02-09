65a0b41f79b875e44eb4c2713c1b6b69f2d7a79e
18648901 <hongmei.li@transsion.com>
2023-02-09 09:46:35 +0800

    补充国包收货与DN同步用例

--- project/DCR/page_object/PurchaseManagement_DistributorReceipt.py
+++ project/DCR/page_object/PurchaseManagement_DistributorReceipt.py
@@ -1,3 +1,5 @@
+import logging
+
 from libs.common.read_element import Element
 from public.base.basics import Base
 from libs.common.time_ui import sleep
@@ -8,19 +10,6 @@ user = Element(pro_name, object_name)
 
 class DitributorReceiptPage(Base):
     """DitributorReceiptPage类"""
-
-    @allure.step("Distributor Receipt页面，进入iframe")
-    def distri_receipt_iframe(self):
-        iframe = self.find_element(user['iframe Distributor Receipt'])
-        self.driver.switch_to.frame(iframe)
-        sleep(2)
-
-    @allure.step("退出iframe")
-    def exit_iframe(self):
-        """退出iframe"""
-        self.driver.switch_to.parent_frame()
-        sleep(2)
-
     @allure.step("点击Unfold 展开更多筛选条件")
     def click_unfold(self):
         """点击Unfold 展开更多筛选条件"""
@@ -28,17 +17,31 @@ class DitributorReceiptPage(Base):
         sleep(2)
 
     @allure.step("国包收货列表页，输入DN条件筛选")
-    def input_dn(self, content):
-        self.input_text(user['DN输入框'], txt=content)
+    def input_dn_query(self, content):
+        self.input_text(user['DN输入框'], content)
 
     @allure.step("勾选列表DN前面第一个复选框")
-    def click_first_chekbox(self):
+    def click_first_checkbox(self):
         self.is_click(user['DN前面第一个复选框'])
 
-    @allure.step("点击Quick Receive 快速收货按钮")
-    def click_quick_receive(self):
+    @allure.step("点击Quick Receive快速收货功能按钮")
+    def click_quick_receive_button(self):
         self.is_click(user['Quick Receive button'])
-        sleep(2)
+        sleep(1.5)
+
+    @allure.step("国包收货页面，一键收货操作后，点击提交按钮")
+    def click_quick_receive_submit(self):
+        self.is_click(user['国包收货Submit'])
+
+    @allure.step("打开快速收货对话框，获取Quick Receive下的DN字段内容")
+    def get_quick_receive_dn(self):
+        get_dn = self.element_text(user['Get Quick Receive DN'])
+        return get_dn
+
+    @allure.step("国包收货页面，一键收货的DN数据")
+    def get_quick_receive_quantity(self):
+        get_quantity = self.element_text(user['Get Quick Receive Quantity'])
+        return get_quantity
 
     @allure.step("获取列表 总数量total Quantity文本")
     def get_list_quantity_text(self):
@@ -62,33 +65,79 @@ class DitributorReceiptPage(Base):
         return dialog_dn
 
     @allure.step("国包收货页面，筛选条件后，点击Search按钮")
-    def click_search(self):
-        self.is_click(user['国包收货列表Search'])
+    def click_search_reset(self, search_reset):
+        self.is_click(user['Search_Reset'], search_reset)
+        sleep(2)
         self.element_text(user['Loading'])
 
-    @allure.step("点击快速收货提交按钮")
-    def click_quick_receive_submit(self):
-        self.is_click_dcr(user['国包收货Submit'])
-        sleep(3)
+    @allure.step("国包收货页面，一键收货的DN数据")
+    def delete_dist_receipt_dn_date(self):
+        logging.info("国包收货后，开始删除收货的DN相关数据")
+        sql1 = SQL('DCR', 'test')
+        inbound_code = sql1.query_db(f"select inbound_code from t_channel_inbound_ticket where order_code = '8100053878'")
+        logging.info("打印查询t_channel_inbound_ticket表inbound_code字段的值:{}".format(inbound_code))
+        sql1.delete_db(f"delete from t_channel_inbound_detail where inbound_code= '{inbound_code}'")
+        sql1.delete_db(f"delete from t_channel_inbound_imei where inbound_code= '{inbound_code}'")
+        sql1.delete_db(f"delete from  t_channel_inbound_ticket where order_code= '8100053878'")
+        sql1.delete_db(f"delete from t_channel_warehouse_current_stock where imei in (select imei1 from t_enter_sap_detail where vbeln='8100053878')")
+        sql1.change_db(f"update t_enter_sap_dn set state=1 where vbeln='8100053878'")
+
+    @allure.step("国包收货页面，删除同步的DN数据")
+    def delete_dist_sync_dn_date(self, dn_data):
+        logging.info("开始删除国包收货 DN同步后的数据")
+        sql2 = SQL('DCR', 'test')
+        sql2.delete_db(f"delete from t_enter_sap_detail where vbeln='{dn_data}'")
+        sql2.delete_db(f"delete from t_enter_sap_dn where vbeln='{dn_data}'")
+
+    #同步DN
+    @allure.step("国包收货页面，点击More Option下的 sync DN同步功能")
+    def click_more_option_sync_dn(self):
+        self.mouse_hover_click(user['More Option'])
+        Base.presence_sleep_dcr(self, user['More Option Sync DN'])
+        self.is_click(user['More Option Sync DN'])
+        sleep(1.5)
+        """断言对话框是否存在Sync DN标题"""
+        get_sync_dn_text = self.element_text(user['Get Sync DN Text'])
+        ValueAssert.value_assert_equal('Sync DN', get_sync_dn_text)
+
+    @allure.step("国包收货页面，打开同步DN页面，输入DN")
+    def input_sync_dn(self, content):
+        self.input_text(user['Sync DN Input DN'], content)
+
+    @allure.step("获取输入框disabled属性是否等于disabled")
+    def get_input_attribute_disabled_state(self, content):
+        get_disabled = self.find_element(user[content])
+        get_disabled1 = get_disabled.get_attribute('disabled')
+        return get_disabled1
+
+    @allure.step("国包收货页面，打开同步DN页面，输入DN后，判断下面的输入框是否为禁用状态，然后点击提交")
+    def click_sync_dn_submit(self):
+        """输入DN后，获取Customer ID、Date、Country输入框是否为disabled禁用状态"""
+        get_customer_id = self.get_input_attribute_disabled_state('Get Customer ID label disabled')
+        logging.info("打印get_customer_id1:{}".format(get_customer_id))
+        get_date = self.get_input_attribute_disabled_state('Get Date label disabled')
+        logging.info("打印get_customer_id1:{}".format(get_customer_id))
+        get_country = self.get_input_attribute_disabled_state('Get Country label disabled')
+        logging.info("打印get_customer_id1:{}".format(get_customer_id))
+        if 'true' in str(get_customer_id) and 'true' in str(get_date) and 'true' in str(get_country):
+            logging.info("打印customer id、date、country输入框为禁用状态，不可输入")
+            """点击同步DN的提交"""
+            self.is_click(user['Sync DN Click Submit'])
 
     @allure.step("国包收货页面，筛选DN收货成功后，获取文本No Data")
     def get_success_text(self):
         get_success = self.element_text(user['Get Successfully Text'])
         return get_success
 
-    @allure.step("国包收货页面，点击Reset按钮")
-    def click_reset(self):
-        self.is_click(user['国包收货Reset'])
-        self.element_text(user['Loading'])
-
     @allure.step("国包收货页面，点击IMEI Detail按钮查看IMEI详情信息")
     def click_imei_detail(self):
         self.presence_sleep_dcr(user['点击IMEI Detail'])
         self.is_click(user['点击IMEI Detail'])
+        sleep(1)
         self.element_text(user['Loading'])
 
     @allure.step("打开IMEI Detail页面，获取DN文本内容")
-    def get_text_imei_detail_DN(self):
+    def get_imei_detail_dn_text(self):
         imei_detail_dn = self.element_text(user['Get IMEI Detail DN'])
         return imei_detail_dn
 
@@ -107,11 +156,99 @@ class DitributorReceiptPage(Base):
     def click_imei_detail_close(self):
         self.is_click(user['Close IMEI Detail'])
 
-    @allure.step("获取下载进度值")
-    def get_download_value(self):
-        attribute = self.get_table_info(user['下载进度条'], attr='aria-valuenow')
+    @allure.step("获取IMEI Detail详情页 下载进度值")
+    def get_imei_detail_download_value(self):
+        attribute = self.get_table_info(user['导出详情下载进度条'], attr='aria-valuenow')
         logging.info('下载进度值是%s' % attribute)
         return int(attribute)
+
+
+    #导入导出
+    def click_import_export(self, import_export):
+        self.is_click(user['import_export'], import_export)
+        sleep(1)
+
+    @allure.step("获取列表导出下载进度值")
+    def get_list_download_value(self):
+        attribute = self.get_table_info(user['导出列表下载进度条'], attr='aria-valuenow')
+        logging.info('下载进度值是%s' % attribute)
+        return int(attribute)
+
+    #单个条件查询
+    @allure.step("Distributor Receipt页面，按Model字段筛选")
+    def input_receipt_model_query(self, content):
+        self.is_click(user['Model点击'])
+        self.input_text(user['Model输入'], content)
+        self.is_click(user['Model_Brand_Category_MarName_Country_Status选择'], content)
+        self.is_click(user['点击label标签'], 'Model')
+
+    @allure.step("Distributor Receipt页面，按material id字段筛选")
+    def input_material_id_query(self, content):
+        self.input_text(user['MaterialID输入'], content)
+
+    @allure.step("Distributor Receipt页面，按sap customer id字段筛选")
+    def input_sap_customer_id_query(self, content):
+        self.input_text(user['SAPCustomerID输入'], content)
+
+    @allure.step("Distributor Receipt页面，按sap_delivery_date字段筛选")
+    def input_sap_delivery_date_query(self, content):
+        self.input_text(user['SAPDeliveryDate开始'], content)
+        self.input_text(user['SAPDeliveryDate结束'], content)
+        self.is_click(user['点击label标签'], 'SAP Delivery Date')
+
+    @allure.step("Distributor Receipt页面，按brand字段筛选")
+    def input_brand_query(self, content):
+        self.is_click(user['Brand点击'])
+        self.is_click(user['下拉框选择值'], content)
+        self.is_click(user['点击label标签'], 'Brand')
+
+    @allure.step("Distributor Receipt页面，按category字段筛选")
+    def input_category_query(self, content, header):
+        self.is_click(user['Category点击'])
+        self.input_text(user['Category输入'], content, header)
+        self.presence_sleep_dcr(user['下拉框选择值'], content)
+        self.is_click(user['下拉框选择值'], content)
+        self.is_click(user['点击label标签'], 'Category')
+
+    @allure.step("Distributor Receipt页面，按imei字段筛选")
+    def input_imei_query(self, content):
+        self.is_click(user['IMEI点击'])
+        self.input_text(user['IMEI输入'], content)
+
+    @allure.step("Distributor Receipt页面，按imei字段筛选")
+    def input_market_name_query(self, content):
+        self.is_click(user['MarketName点击'])
+        self.input_text(user['MarketName输入'], content)
+        self.presence_sleep_dcr(user['下拉框选择值'], content)
+        self.is_click(user['下拉框选择值'], content)
+        self.is_click(user['点击label标签'], 'Market Name')
+
+    @allure.step("Distributor Receipt页面，按country字段筛选")
+    def input_country_query(self, content, header):
+        self.is_click(user['Country点击'])
+        self.input_text(user['Country输入'], content)
+        self.presence_sleep_dcr(user['下拉框选择值'], content)
+        self.is_click(user['下拉框选择值'], content, header)
+        self.is_click(user['点击label标签'], 'Country')
+
+    @allure.step("Distributor Receipt页面，按status字段筛选")
+    def input_status_query(self, content, content2, content3, header):
+        self.is_click(user['Status点击'])
+        self.is_click(user['下拉框选择值'], content, header)
+        self.is_click(user['下拉框选择值'], content2, header)
+        self.is_click(user['下拉框选择值'], content3, header)
+        self.is_click(user['点击label标签'], 'Status')
+
+
+    @allure.step("断言：列表页面查询结果")
+    def assert_query_result(self, header, content):
+        logging.info('开始断言：页面查询结果')
+        DomAssert(self.driver).assert_search_contains_result(user['menu表格字段'], user['表格内容'], header, content, sc_element=user['滚动条'], h_element=user['表头文本'])
+
+    @allure.step("断言：页面查询结果")
+    def assert_query_imei_detail_result(self, header, content):
+        logging.info('开始断言：页面查询结果')
+        DomAssert(self.driver).assert_search_contains_result(user['menu表格字段'], user['表格内容'], header, content, h_element=user['IMEI Detail表头文本'])
 
 
 
@@ -170,10 +307,10 @@ class DistributorReceiptQuery(Base):
     @allure.step("点击Unfold展开筛选项按钮")
     def click_button(self, txt):
         self.is_click(user['Unfold_Search_Reset按钮'], txt)
-        if txt=='Search':
-            sleep(5)
+        if txt == 'Search':
+            self.element_text(user['Loading'])
         elif txt == 'Reset':
-            sleep(3)
+            self.element_text(user['Loading'])
         else:
             sleep()
 
@@ -184,6 +321,7 @@ class DistributorReceiptQuery(Base):
             self.is_click(user['Model点击'])
             self.input_text(user['Model输入'], txt=content)
             self.is_click(user['Model_Brand_Category_MarName_Country_Status选择'], content)
+            self.is_click(user['点击label标签'], 'Model')
         elif type == 'Customer':
             self.is_click(user['Customer点击'])
             self.input_text(user['Customer输入'], txt=content)
@@ -200,7 +338,7 @@ class DistributorReceiptQuery(Base):
             self.input_text(user['SAPCustomerID输入'], txt=content)
         elif type == 'Brand':
             self.is_click(user['Brand点击'])
-            self.is_click(user['Model_Brand_Category_MarName_Country_Status选择'], content)
+            self.is_click(user['下拉框选择值'], content)
             self.click_dn()
         elif type == 'Category':
             self.is_click(user['Category点击'])
@@ -234,6 +372,8 @@ class DistributorReceiptQuery(Base):
         else:
             logging.info('type is wrong,pls check')
         sleep()
+
+
 
 
 if __name__ == '__main__':
